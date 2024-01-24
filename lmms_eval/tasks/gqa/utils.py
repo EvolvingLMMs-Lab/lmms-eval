8114a24e504d735c2bc9728da9d88482e858c5eb
Li Bo <drluodian@gmail.com>
2024-01-24 10:00:33 +0800

    [Datasets] Add four internal evaluation datasets (#13)
    * Update generation_kwargs in pope.yaml
    
    * Update pope_doc_to_text function
    
    * Remove unused variable in mmvet_process_results function
    
    * Remove unused imports in utils.py
    
    * Refactor get_chat_response function to include retries for API requests
    
    * Update gpt_eval_model_name in lmms_eval/tasks/dc100_en.yaml and add retry logic in get_chat_response function
    
    * Update prompt variable in lmms_eval tasks
    
    * Refactor output_name variable in cli_evaluate function
    
    * Fix logging message in mmvet_process_results function
    
    * Update sleep time in get_chat_response function
    
    * Merge commit 'c191ba541ee9e94336c4cb60f3cf495ca560129a'
    
    * Refactor get_eval function to include retries
    
    * Add token parameter to load_dataset function in gqa_doc_to_visual
    
    * Refactor llava_process_results and llava_aggregation functions

--- lmms_eval/tasks/gqa/utils.py
+++ lmms_eval/tasks/gqa/utils.py
@@ -9,7 +9,7 @@ def gqa_doc_to_visual(doc):
     global GQA_RAW_IMAGE_DATASET
     global GQA_ID2IMAGE
     if GQA_RAW_IMAGE_DATASET is None:
-        GQA_RAW_IMAGE_DATASET = load_dataset("lmms-lab/GQA", "testdev_balanced_images", split="testdev")
+        GQA_RAW_IMAGE_DATASET = load_dataset("lmms-lab/GQA", "testdev_balanced_images", split="testdev", token=True)
         GQA_ID2IMAGE = {}
         for row in GQA_RAW_IMAGE_DATASET:
             GQA_ID2IMAGE[row["id"]] = row["image"].convert("RGB")
