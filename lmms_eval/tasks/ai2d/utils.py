6904a358f119dd69ee2b495b3b1f16a7f04e17ac
Zhang Peiyuan <a1286225768@gmail.com>
2024-01-27 13:56:37 +0800

    Dev/add chartqa and ai2d (#23)
    * add mmme
    
    * black
    
    * add model specific prompt and gen kwargs
    
    * black
    
    * add yaml config to supprot multi-model eval
    
    * print table at the end
    
    * refactor multi model code
    
    * add chartqa
    
    * black
    
    * add ai2d
    
    * black
    
    * update chartqa
    
    * blacl
    
    * update ai2d dataset
    
    * black
    
    * Add 'submissions/' directory to .gitignore
    
    * Add Python setup and Black version installation workflow
    Refactor ContextSampler class in samplers.py
    Remove unnecessary line in DecontaminationFilter class
    Update dependencies in pyproject.toml
    
    * Refactor code in ContextSampler class
    
    ---------
    
    Co-authored-by: Bo Li <drluodian@gmail.com>

---
+++ lmms_eval/tasks/ai2d/utils.py
@@ -0,0 +1,32 @@
+def ai2d_doc_to_text(doc, model_specific_prompt_kwargs=None):
+    question, choices = doc["question"], doc["options"]
+    len_choices = len(choices)
+    options = [chr(ord("A") + i) for i in range(len_choices)]
+    choices_str = "\n".join([f"{option}. {choice}" for option, choice in zip(options, choices)])
+
+    post_prompt = model_specific_prompt_kwargs["post_prompt"]
+    pre_prompt = model_specific_prompt_kwargs["pre_prompt"]
+    return f"{pre_prompt}{question}\n{choices_str}{post_prompt}"
+
+
+def ai2d_doc_to_visual(doc):
+    return [doc["image"].convert("RGB")]
+
+
+def ai2d_doc_to_target(doc):
+    len_choices = len(doc["options"])
+    options = [chr(ord("A") + i) for i in range(len_choices)]
+    return options[int(doc["answer"])]
+
+
+def ai2d_process_results(doc, results):
+    # I know this is weird, but it's how llava parse it.
+    target = ai2d_doc_to_target(doc)
+    pred = results[0]
+    if pred == target:
+        return {"exact_match": 1.0}
+    # pattern: ^[A-Z]\. .*
+    if len(pred) >= 2 and pred[0].isupper() and pred[1] == ".":
+        result = 1.0 if pred[0] == target else 0.0
+        return {"exact_match": result}
+    return {"exact_match": 0.0}
