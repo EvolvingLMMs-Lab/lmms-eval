891de49f29654498dffc2ce37582f69b0682320c
Zhang Peiyuan <a1286225768@gmail.com>
2024-01-28 02:04:57 +0800

    Add InfoVQA, DocVQA, and QwenVL (#28)
    * add mmme
    
    * black
    
    * add model specific prompt and gen kwargs
    
    * black
    
    * add yaml config to supprot multi-model eval
    
    * print table at the end
    
    * refactor multi model code
    
    * add chartqa
    
    * black
    
    * add ai2d
    
    * black
    
    * update chartqa
    
    * blacl
    
    * update ai2d dataset
    
    * black
    
    * add qwenvl
    
    * add infovqa and docvqa

--- lmms_eval/models/__init__.py
+++ lmms_eval/models/__init__.py
@@ -1,2 +1,3 @@
 from .llava import Llava
 from .otterhd import OtterHD
+from .qwen_vl import Qwen_VL
---
+++ lmms_eval/tasks/docvqa/utils.py
@@ -0,0 +1,26 @@
+import json
+
+
+def docvqa_doc_to_visual(doc):
+    return [doc["image"].convert("RGB")]
+
+
+def docvqa_doc_to_text(doc, mdoel_specific_prompt_kwargs):
+    question = doc["question"]
+    pre_prompt = mdoel_specific_prompt_kwargs["pre_prompt"]
+    post_prompt = mdoel_specific_prompt_kwargs["post_prompt"]
+    return f"{pre_prompt}{question}{post_prompt}"
+
+
+def docvqa_test_process_results(doc, results):
+    pred = results[0]
+    questionId = doc["questionId"]
+    return {"anls": {"questionId": int(questionId), "answer": pred}}
+
+
+def docvqa_test_aggregate_results(results):
+    # save results as json
+
+    with open("docvqa_test_for_submission.json", "w") as f:
+        json.dump(results, f)
+    return -1
