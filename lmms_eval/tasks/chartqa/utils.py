38eb0f316ce90b5dfd262a68b0b4c955a554199b
Zhang Peiyuan <a1286225768@gmail.com>
2024-02-03 14:06:53 +0800

    adapt qwen to sqa, gqa, ai2d, docvqa (#36)
    * adapt qwen to sqa, gqa, ai2d, docvqa
    
    * black

--- lmms_eval/tasks/chartqa/utils.py
+++ lmms_eval/tasks/chartqa/utils.py
@@ -2,10 +2,10 @@ def chartqa_doc_to_visual(doc):
     return [doc["image"].convert("RGB")]
 
 
-def chartqa_doc_to_text(doc, mdoel_specific_prompt_kwargs):
+def chartqa_doc_to_text(doc, model_specific_prompt_kwargs):
     question = doc["question"]
-    pre_prompt = mdoel_specific_prompt_kwargs["pre_prompt"]
-    post_prompt = mdoel_specific_prompt_kwargs["post_prompt"]
+    pre_prompt = model_specific_prompt_kwargs["pre_prompt"]
+    post_prompt = model_specific_prompt_kwargs["post_prompt"]
     return f"{pre_prompt}{question}{post_prompt}"
 
 
