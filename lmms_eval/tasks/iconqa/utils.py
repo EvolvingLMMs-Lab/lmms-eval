1d12a9b5d9437b544f264a133ca759482311557c
Li Bo <drluodian@gmail.com>
2024-02-13 19:02:18 +0800

    [Model] Add models (#47)
    * Refactor logging and model initialization
    
    * Fix wandb_logger.online() method call
    
    * Add error handling during evaluation
    
    * Add wait time and error handling in get_chat_response function
    
    * Update wait_time in get_chat_response function
    
    * Refactor code for improved readability and maintainability
    
    * Refactor doc_to_visual function to handle multiple images in ICON-QA tasks
    
    * Refactor logging_utils.py and utils.py
    
    This commit refactors the `logging_utils.py` and `utils.py` files. It removes unused imports, adjusts code formatting, and updates the `get_chat_response` function to increase the `wait_time` parameter from 5 to 10.
    
    * Refactor code for wandb logging and generation in OtterHD class
    
    * Refactor prepare_report_by_task method in logging_utils.py
    
    * Update generation parameters in OtterHD model
    
    * Update generation parameters in OtterHD model
    
    * Squashed commit of the following:
    
    commit 3427ed979bf5407f09922f15f47c5f43edb3cf12
    Author: kcz358 <92624596+kcz358@users.noreply.github.com>
    Date:   Tue Feb 13 18:50:37 2024 +0800
    
        Fix seedbench choices bugs (#45)
    
    commit 71717897ebb0b522b60df713e7c9022e50b2585a
    Author: XinrunDu <154438029+XinrunDu@users.noreply.github.com>
    Date:   Tue Feb 13 18:50:23 2024 +0800
    
        add stvqa and multidocvqa (#46)
    
    commit a39cbb726a183ab439accdb61e48108a35a7963a
    Author: XinrunDu <154438029+XinrunDu@users.noreply.github.com>
    Date:   Sun Feb 11 00:54:39 2024 +0800
    
        add cmmmu (#44)
    
        Co-authored-by: ygjin11 <1633504509@qq.com>
    
    commit 26d1427ad85479ecd368bb20f8e0f4075cfc43c8
    Author: kcz358 <92624596+kcz358@users.noreply.github.com>
    Date:   Sun Feb 11 00:54:23 2024 +0800
    
        [Feat] Add qwen loglikelihood (#43)
    
        * Add qwen loglikelihood
    
        * Revise the pyproject dependency. Move tiktoken out from optional-dependencies
    
        * Add ferret-bench
    
        * Add seedbench 2, test on llava
    
    commit c070baf9d3c0c7561d5a33b289a1dc0539f11eb1
    Author: JvThunder <44111143+JvThunder@users.noreply.github.com>
    Date:   Wed Feb 7 00:08:22 2024 +0800
    
        Joshua/vizwizvqa refactor (#42)
    
        * refactor vizwizvqa task
    
        * Merge commit '59c7d67077c315657a02bdee2eace0e64c1ee0d4'
    
        * Fix exact_match accuracy calculation in vizwiz_vqa_process_results
    
        * Update vizwiz_vqa tasks
    
        ---------
    
        Co-authored-by: Fanyi Pu <FPU001@e.ntu.edu.sg>

---
+++ lmms_eval/tasks/iconqa/utils.py
@@ -0,0 +1,62 @@
+import json
+import os
+
+
+def options_to_str(options_prompt):
+    option_prompt_str = ""
+    for i, option in enumerate(options_prompt):
+        option_choice = chr(ord("A") + i)
+        option_prompt_str += f"{option_choice}. {option}\n"
+
+    option_prompt_str = option_prompt_str.rstrip("\n")
+    return option_prompt_str
+
+
+def doc_to_visual(doc):
+    image_list = []
+    if "query_image" in doc:
+        image_list.append(doc["query_image"].convert("RGB"))
+    if "choice_image_0" in doc:
+        image_list.append(doc["choice_image_0"].convert("RGB"))
+    if "choice_image_1" in doc:
+        image_list.append(doc["choice_image_1"].convert("RGB"))
+    if "choice_image_2" in doc:
+        image_list.append(doc["choice_image_2"].convert("RGB"))
+    if "choice_image_3" in doc:
+        image_list.append(doc["choice_image_3"].convert("RGB"))
+    if "choice_image_4" in doc:
+        image_list.append(doc["choice_image_4"].convert("RGB"))
+    assert len(image_list) < 6, "Maximum 5 images allowed for ICON-QA"
+    return image_list
+
+
+def doc_to_text(doc, model_specific_prompt_kwargs):
+    question = doc["question"]
+    ques_type = doc["ques_type"]
+    options_prompt = []
+
+    if ques_type == "choose_img":
+        options_prompt.append("The first image.")
+        options_prompt.append("The second image.")
+
+        options_str = options_to_str(options_prompt)
+        full_prompt = f"{model_specific_prompt_kwargs['pre_prompt']}{model_specific_prompt_kwargs['statement']}{model_specific_prompt_kwargs['options_statement'].format(question=question, options=options_str)}"
+
+    elif ques_type == "choose_txt":
+        choices = doc["choices"].split(",")
+        for i, choice in enumerate(choices):
+            options_prompt.append(f"{choice}")
+
+        options_str = options_to_str(options_prompt)
+        full_prompt = f"{model_specific_prompt_kwargs['pre_prompt']}{model_specific_prompt_kwargs['statement']}{model_specific_prompt_kwargs['options_statement'].format(question=question, options=options_str)}"
+
+    elif ques_type == "fill_in_blank":
+        full_prompt = f"{model_specific_prompt_kwargs['pre_prompt']}{model_specific_prompt_kwargs['statement']}{model_specific_prompt_kwargs['freeform_statement'].format(question=question)}"
+
+    return full_prompt
+
+
+def test_process_results(doc, results):
+    pred = results[0]
+    questionId = doc["questionId"]
+    return {"anls": {"questionId": int(questionId), "answer": pred}}
