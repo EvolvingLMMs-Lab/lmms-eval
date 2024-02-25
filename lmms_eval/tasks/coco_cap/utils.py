7fa200553d2484c4a087a2ec6f1504247f648917
Pu Fanyi <FPU001@e.ntu.edu.sg>
2024-02-26 01:15:53 +0800

    Dataset Refractor (#50)
    * refactor vizwizvqa task
    
    * Delete vqav2_test and vqav2_val YAML files
    
    * Refactor vqav2_process_results functions
    
    * Add a pack for vqav2
    
    * refactor okvqa
    
    * roll back vizwiz_vqa
    
    * Fix exact_match calculation in ok_vqa_process_results
    
    * Update OKVQA dataset name in readme
    
    * add model_specific_prompt_kwargs
    
    * add model_specific_prompt_kwargs to vizwiz_vqa
    
    * add model_specific_prompt_kwargs for vqav2
    
    * lint
    
    * fix a small bug for eval_logger
    
    * Refactor make_table function to display points as "  -  " if value is None
    
    * Merge commit '63fc8eee4dddfbe741e5a862e5ff30d19c34238e'
    
    * Refactor ok_vqa_aggreate_submissions function
    
    * Merge commit 'd16bbce134d453c624834e090af1e0f869fdde15'
    
    * Refactor VQA submission file saving
    
    * Update file utils
    
    * Merge commit '7332704263a45ab6fa69aad0c4303cd9cbc26813'
    
    * Refactor file path handling and submission generation
    
    * OKVQA path
    
    * vizwizvqa file
    
    * pack cmmmu
    
    * fix a small metric bug for cmmmu
    
    * Add higher_is_better flag to submission metric
    
    * Add CMMMU dataset to README.md
    
    * Add logging and refactor submission file generation in docvqa utils.py
    
    * pack docvqa
    
    * add traceback to print detailed error
    
    * Refactor docvqa_test_aggregate_results to accept additional arguments
    
    * Add metric check in evaluator.py and update test.yaml and val.yaml
    
    * add common `EvalAIAnswerProcessor` for okvqa, textvqa, vizwizvqa and vqav2
    
    * merge textvqa
    
    * textvqa
    
    * Modify submission file generation for COCO test results
    
    * Update test result storage path
    
    * update coco cap file name
    
    * Update COCO 2017 Caption dataset name
    
    * ferret
    
    * Add Ferret dataset
    
    * Refactor hb_doc_to_text function to include model-specific prompts
    
    * Add IconQA and its subtasks
    
    * Refactor image list creation in doc_to_visual function
    
    * Add process_results function to default template
    
    * Update process_results function in iconqa utils.py
    
    * refactor flickr30k
    
    * change aggregation function
    
    * Fix formatting issues and update logging message
    
    * Fix llava can not handle only text question (no visuals)
    
    * Fix qwen can not handle no image question (no visuals)
    
    * Add fuyu prepare accelerator scripts
    
    * refactor mme
    
    * naming consistency
    
    * aggregation_submissions consistency
    
    * flickr30k naming consistency
    
    * remove submissions for mme
    
    * remove unused submission function
    
    * Refactor infovqa_test.yaml and infovqa_val.yaml
    
    * Refactor code for improved readability and maintainability
    
    * stvqa
    
    * remane sqa
    
    * Update lmms_eval textcaps files and utils.py
    
    * Update default prompt for text captions
    
    * Refactor textcaps_aggregation_result function
    
    * Add generate_submission_file function and update mathvista_aggregate_results signature
    
    * Update nocaps_test.yaml and nocaps_val.yaml
    
    * refractor internal_eval
    
    * Add internal evaluation datasets
    
    * pack multidocvqa
    
    * mmvet
    
    * Fix gpt eval timeout issue for hallubench, restore load from gpt to avoid re evaluating
    
    * Refractor llava wild
    
    * Refractor llava-bench-coco
    
    * Add JSON file generation for gpt evaluation details
    
    * mmmu
    
    * Remove MMBench English and Chinese tasks
    
    * Remove unnecessary return statement in mmbench_aggregate_test_results function
    
    * Fix distributed process group initialization
    
    * Update dataset paths and group names in mmbench test configs
    
    * Update import statements in cc_utils.py, cn_utils.py, and en_utils.py
    
    * Add torch module import
    
    * lint
    
    * Remove IconQA dataset from README.md
    
    * Add Multi-DocVQA and its submodules
    
    * Add new datasets and update task names
    
    ---------
    
    Co-authored-by: JvThunder <joshuaadrianc@gmail.com>
    Co-authored-by: kcz358 <kaichenzhang358@outlook.com>

---
+++ lmms_eval/tasks/coco_cap/utils.py
@@ -0,0 +1,157 @@
+import os
+import json
+from pycocoevalcap.eval import COCOEvalCap, Bleu, Meteor, Rouge, Cider, Spice
+from pycocoevalcap.tokenizer.ptbtokenizer import PTBTokenizer
+from pycocotools.coco import COCO
+
+from lmms_eval.tasks._task_utils.file_utils import generate_submission_file
+
+import logging
+
+eval_logger = logging.getLogger("lmms-eval")
+
+dir_name = os.path.dirname(os.path.abspath(__file__))
+
+COCO_METRICS = ["Bleu_4", "Bleu_3", "Bleu_2", "Bleu_1", "METEOR", "ROUGE_L", "CIDEr"]  # , "SPICE"]
+
+
+def coco_doc_to_visual(doc):
+    return [doc["image"].convert("RGB")]
+
+
+def coco_doc_to_text(doc):
+    return f"Provide a one-sentence caption for the provided image."
+
+
+def coco_process_result(doc, result):
+    """
+    Args:
+        doc: a instance of the eval dataset
+        results: [pred]
+    Returns:
+        a dictionary with key: metric name, value: metric value
+    """
+    pred = result[0] if len(result) > 0 else ""
+    question_id = doc["question_id"]
+    # The question id in our dataset is the image file itself
+    image_id = int(question_id.split("_")[-1].split(".")[0])
+    id = doc["id"]
+
+    data_dict = {"answer": doc["answer"], "pred": pred, "image_id": image_id, "id": id}
+
+    return {f"coco_{metric}": data_dict for metric in COCO_METRICS}
+
+
+def coco_aggregation_result(results, metric):
+    scorers = [(Bleu(4), "Bleu_1"), (Bleu(4), "Bleu_2"), (Bleu(4), "Bleu_3"), (Bleu(4), "Bleu_4"), (Meteor(), "METEOR"), (Rouge(), "ROUGE_L"), (Cider(), "CIDEr"), (Spice(), "SPICE")]
+    scorers_dict = {s[1]: s for s in scorers}
+
+    stored_results = []
+    # In order to make the coco eval tools to successfully create index
+    # We need at least two dict in the dataset
+    # 'annotation' and 'images'
+    # 'annotation' exactly reproduce the original annotation
+    # 'images' however only need the image id which is contained in the file name
+    dataset = {"annotations": [], "images": []}
+    idx = 0
+    for result in results:
+        stored_results.append({"image_id": int(result["image_id"]), "caption": result["pred"]})
+        for a in result["answer"]:
+            dataset["annotations"].append({"image_id": int(result["image_id"]), "caption": a, "id": idx})
+            idx += 1
+        dataset["images"].append({"id": result["image_id"]})
+
+    coco = COCO()
+    # Manually create index here
+    coco.dataset = dataset
+    coco.createIndex()
+
+    coco_result = coco.loadRes(stored_results)
+    coco_eval = COCOEvalCap(coco, coco_result)
+
+    imgIds = coco_eval.params["image_id"]
+    gts = {}
+    res = {}
+    for imgId in imgIds:
+        gts[imgId] = coco_eval.coco.imgToAnns[imgId]
+        res[imgId] = coco_eval.cocoRes.imgToAnns[imgId]
+
+    eval_logger.info("tokenization...")
+    tokenizer = PTBTokenizer()
+    gts = tokenizer.tokenize(gts)
+    res = tokenizer.tokenize(res)
+
+    eval_logger.info(f"Computing {metric} scores...")
+
+    score, scores = scorers_dict[metric][0].compute_score(gts, res)
+    # When metric is one of the Bleu, score will be a list
+    if type(score) == list:
+        n = int(metric.split("_")[-1])
+        score = score[n - 1]
+
+    os.makedirs("./submissions", exist_ok=True)
+    if not os.path.exists("./submissions/coco_captions_val2014_alg_results.json"):
+        eval_logger.info("Storing prediction that can be submitted to the server ...")
+        with open("./submissions/coco_captions_val2014_alg_results.json", "w") as f:
+            json.dump(stored_results, f, indent=4)
+
+    return score
+
+
+def coco_bleu4(results):
+    return coco_aggregation_result(results, "Bleu_4")
+
+
+def coco_bleu3(results):
+    return coco_aggregation_result(results, "Bleu_3")
+
+
+def coco_bleu2(results):
+    return coco_aggregation_result(results, "Bleu_2")
+
+
+def coco_bleu1(results):
+    return coco_aggregation_result(results, "Bleu_1")
+
+
+def coco_meteor(results):
+    return coco_aggregation_result(results, "METEOR")
+
+
+def coco_rougel(results):
+    return coco_aggregation_result(results, "ROUGE_L")
+
+
+def coco_cider(results):
+    return coco_aggregation_result(results, "CIDEr")
+
+
+def coco_spice(results):
+    return coco_aggregation_result(results, "SPICE")
+
+
+def coco_test_process_result(doc, result):
+    """
+    Args:
+        doc: a instance of the eval dataset
+        results: [pred]
+    Returns:
+        a dictionary with key: metric name (in this case coco_passthrough), value: metric value
+    """
+    question_id = doc["question_id"]
+    # The question id in our dataset is the image file itself
+    image_id = int(question_id.split("_")[-1].split(".")[0])
+    return {"coco_passthrough": {"pred": result, "image_id": image_id}}
+
+
+def coco_test_aggregation_result(results, args):
+    stored_results = []
+    for result in results:
+        stored_results.append({"image_id": int(result["image_id"]), "caption": result["pred"]})
+
+    path = generate_submission_file("coco_captions_test2014_alg_results.json", args)
+    eval_logger.info("Storing prediction that can be submitted to the server ...")
+    with open(path, "w") as f:
+        json.dump(stored_results, f, indent=4)
+
+    eval_logger.info(f"Your test result has been stored in to {path}. Make sure you also have the val result stored to submit to the server on https://codalab.lisn.upsaclay.fr/competitions/7404#participate.")
