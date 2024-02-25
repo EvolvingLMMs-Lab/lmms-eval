acd94cbd4d6aef06ffff90c827500c9ef926b85e
Pu Fanyi <FPU001@e.ntu.edu.sg>
2024-02-26 01:15:53 +0800

    Dataset Refractor (#50)
    * refactor vizwizvqa task
    
    * Delete vqav2_test and vqav2_val YAML files
    
    * Refactor vqav2_process_results functions
    
    * Add a pack for vqav2
    
    * refactor okvqa
    
    * roll back vizwiz_vqa
    
    * Fix exact_match calculation in ok_vqa_process_results
    
    * Update OKVQA dataset name in readme
    
    * add model_specific_prompt_kwargs
    
    * add model_specific_prompt_kwargs to vizwiz_vqa
    
    * add model_specific_prompt_kwargs for vqav2
    
    * lint
    
    * fix a small bug for eval_logger
    
    * Refactor make_table function to display points as "  -  " if value is None
    
    * Merge commit '63fc8eee4dddfbe741e5a862e5ff30d19c34238e'
    
    * Refactor ok_vqa_aggreate_submissions function
    
    * Merge commit 'd16bbce134d453c624834e090af1e0f869fdde15'
    
    * Refactor VQA submission file saving
    
    * Update file utils
    
    * Merge commit '7332704263a45ab6fa69aad0c4303cd9cbc26813'
    
    * Refactor file path handling and submission generation
    
    * OKVQA path
    
    * vizwizvqa file
    
    * pack cmmmu
    
    * fix a small metric bug for cmmmu
    
    * Add higher_is_better flag to submission metric
    
    * Add CMMMU dataset to README.md
    
    * Add logging and refactor submission file generation in docvqa utils.py
    
    * pack docvqa
    
    * add traceback to print detailed error
    
    * Refactor docvqa_test_aggregate_results to accept additional arguments
    
    * Add metric check in evaluator.py and update test.yaml and val.yaml
    
    * add common `EvalAIAnswerProcessor` for okvqa, textvqa, vizwizvqa and vqav2
    
    * merge textvqa
    
    * textvqa
    
    * Modify submission file generation for COCO test results
    
    * Update test result storage path
    
    * update coco cap file name
    
    * Update COCO 2017 Caption dataset name
    
    * ferret
    
    * Add Ferret dataset
    
    * Refactor hb_doc_to_text function to include model-specific prompts
    
    * Add IconQA and its subtasks
    
    * Refactor image list creation in doc_to_visual function
    
    * Add process_results function to default template
    
    * Update process_results function in iconqa utils.py
    
    * refactor flickr30k
    
    * change aggregation function
    
    * Fix formatting issues and update logging message
    
    * Fix llava can not handle only text question (no visuals)
    
    * Fix qwen can not handle no image question (no visuals)
    
    * Add fuyu prepare accelerator scripts
    
    * refactor mme
    
    * naming consistency
    
    * aggregation_submissions consistency
    
    * flickr30k naming consistency
    
    * remove submissions for mme
    
    * remove unused submission function
    
    * Refactor infovqa_test.yaml and infovqa_val.yaml
    
    * Refactor code for improved readability and maintainability
    
    * stvqa
    
    * remane sqa
    
    * Update lmms_eval textcaps files and utils.py
    
    * Update default prompt for text captions
    
    * Refactor textcaps_aggregation_result function
    
    * Add generate_submission_file function and update mathvista_aggregate_results signature
    
    * Update nocaps_test.yaml and nocaps_val.yaml
    
    * refractor internal_eval
    
    * Add internal evaluation datasets
    
    * pack multidocvqa
    
    * mmvet
    
    * Fix gpt eval timeout issue for hallubench, restore load from gpt to avoid re evaluating
    
    * Refractor llava wild
    
    * Refractor llava-bench-coco
    
    * Add JSON file generation for gpt evaluation details
    
    * mmmu
    
    * Remove MMBench English and Chinese tasks
    
    * Remove unnecessary return statement in mmbench_aggregate_test_results function
    
    * Fix distributed process group initialization
    
    * Update dataset paths and group names in mmbench test configs
    
    * Update import statements in cc_utils.py, cn_utils.py, and en_utils.py
    
    * Add torch module import
    
    * lint
    
    * Remove IconQA dataset from README.md
    
    * Add Multi-DocVQA and its submodules
    
    * Add new datasets and update task names
    
    ---------
    
    Co-authored-by: JvThunder <joshuaadrianc@gmail.com>
    Co-authored-by: kcz358 <kaichenzhang358@outlook.com>

---
+++ lmms_eval/tasks/ok_vqa/_generate_config.py
@@ -0,0 +1,25 @@
+import os
+import yaml
+
+splits = ["val2014"]
+tasks = ["vqa"]
+
+if __name__ == "__main__":
+    dump_tasks = []
+    for task in tasks:
+        for split in splits:
+            yaml_dict = {"group": f"ok_vqa", "task": f"ok_vqa_{split}", "include": f"_default_template_{task}_yaml", "test_split": split}
+            if split == "train":
+                yaml_dict.pop("group")
+            else:
+                dump_tasks.append(f"ok_vqa_{split}")
+
+            save_path = f"./ok_vqa_{split}.yaml"
+            print(f"Saving to {save_path}")
+            with open(save_path, "w") as f:
+                yaml.dump(yaml_dict, f, default_flow_style=False, sort_keys=False)
+
+    group_dict = {"group": "ok_vqa", "task": dump_tasks}
+
+    with open("./_ok_vqa.yaml", "w") as f:
+        yaml.dump(group_dict, f, default_flow_style=False, indent=4)
