88aa2ed6a5cf24d94286a42a6f6e607ad7f24a98
Bo Li <bo.li01@bytedance.com>
2024-03-07 12:55:56 +0000

    Imported faster model download modules

--- lmms_eval/models/__init__.py
+++ lmms_eval/models/__init__.py
@@ -1,14 +1,13 @@
-from .llava import Llava
-from .otterhd import OtterHD
-from .qwen_vl import Qwen_VL
-from .fuyu import Fuyu
-from .gpt4v import GPT4V
-from .instructblip import InstructBLIP
-from .minicpm_v import MiniCPM_V
 import os
 
 try:
     # enabling faster model download
+    from .llava import Llava
+    from .qwen_vl import Qwen_VL
+    from .fuyu import Fuyu
+    from .gpt4v import GPT4V
+    from .instructblip import InstructBLIP
+    from .minicpm_v import MiniCPM_V
     import hf_transfer
 
     os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
