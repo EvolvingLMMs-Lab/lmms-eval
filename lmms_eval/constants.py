178ca0c26d6a53091e986768c01cea2b2037fe5b
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2024-04-08 19:41:21 +0000

    Fix chat templates

--- lmms_eval/constants.py
+++ lmms_eval/constants.py
@@ -1,2 +1,4 @@
 DEFAULT_IMAGE_TOKEN = "<image>"
-DEFAULT_CHAT_TEMPLATE = "{% for message in messages %}{% if message['role'] == 'user' %}USER: {{ message['content'] }}\n{% else %}ASSISTANT: {{ message['content'] }}\n{% endif %}{% endfor %}"
+
+# Default chat templates
+VICUNA_CHAT_TEMPLATE = "{% for message in messages %}{% if loop.index0 == 0 %}A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: {{ message['content'] }} {% elif message['role'] == 'user' %}USER: {{ message['content'] }} {% else %} ASSISTANT: {{ message['content'] }}{{ eos_token }}{% endif %}{% endfor %}{% if add_generation_prompt %}{{ 'ASSISTANT:' }}{% endif %}"
