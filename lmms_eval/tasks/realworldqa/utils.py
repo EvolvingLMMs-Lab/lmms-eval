390c7970f0ce2f1bf7388837d086482b23a399bb
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-04-13 15:16:26 +0800

    realworldqa

---
+++ lmms_eval/tasks/realworldqa/utils.py
@@ -0,0 +1,14 @@
+def realworldqa_doc_to_visual(doc):
+    return [doc["image"].convert("RGB")]
+
+
+def realworldqa_doc_to_text(doc, model_specific_prompt_kwargs=None):
+    if model_specific_prompt_kwargs is None:
+        model_specific_prompt_kwargs = {}
+    pre_prompt = ""
+    post_prompt = ""
+    if "pre_prompt" in model_specific_prompt_kwargs:
+        pre_prompt = model_specific_prompt_kwargs["pre_prompt"]
+    if "post_prompt" in model_specific_prompt_kwargs:
+        post_prompt = model_specific_prompt_kwargs["post_prompt"]
+    return f"{pre_prompt}{doc['question']}{post_prompt}"
