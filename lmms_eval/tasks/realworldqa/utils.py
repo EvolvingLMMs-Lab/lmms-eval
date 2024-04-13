afc9915dbfc5eac2d85408c381a5b02060d063f2
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-04-13 15:40:45 +0800

    update postprompt

--- lmms_eval/tasks/realworldqa/utils.py
+++ lmms_eval/tasks/realworldqa/utils.py
@@ -1,3 +1,6 @@
+REPLACE_PROMPT = "Please answer directly with only the letter of the correct option and nothing else."
+
+
 def realworldqa_doc_to_visual(doc):
     return [doc["image"].convert("RGB")]
 
@@ -7,8 +10,10 @@ def realworldqa_doc_to_text(doc, model_specific_prompt_kwargs=None):
         model_specific_prompt_kwargs = {}
     pre_prompt = ""
     post_prompt = ""
+    question = doc["question"].strip()
     if "pre_prompt" in model_specific_prompt_kwargs:
         pre_prompt = model_specific_prompt_kwargs["pre_prompt"]
-    if "post_prompt" in model_specific_prompt_kwargs:
+    if "post_prompt" in model_specific_prompt_kwargs and model_specific_prompt_kwargs["post_prompt"]:
+        question.replace(REPLACE_PROMPT, "")
         post_prompt = model_specific_prompt_kwargs["post_prompt"]
-    return f"{pre_prompt}{doc['question']}{post_prompt}"
+    return f"{pre_prompt}{question}{post_prompt}"
