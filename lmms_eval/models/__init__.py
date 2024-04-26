8159d569e3a02c41694b8b7bb24e4b9284e336b5
Kaichen Zhang - NTU <kaichenzhang358@outlook.com>
2024-04-26 14:47:28 +0800

    [Merge] git checkout --patch sglang form public (#80)
    * Merge llava sglang from external
    
    * Merge public main sglang
    
    * Keep image in rolling

--- lmms_eval/models/__init__.py
+++ lmms_eval/models/__init__.py
@@ -13,7 +13,8 @@ AVAILABLE_MODELS = {
     "video_llava": "VideoLLaVA",
     "xcomposer2_4KHD": "XComposer2_4KHD",
     "claude" : "Claude",
-    "qwen_vl_api" : "Qwen_VL_API"
+    "qwen_vl_api" : "Qwen_VL_API",
+    "llava_sglang" : "LlavaSglang",
 }
 
 for model_name, model_class in AVAILABLE_MODELS.items():
