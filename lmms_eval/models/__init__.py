9ee68f6af95c5f2aedbedec4b6162f41bfb05dfb
Bo Li <drluodian@gmail.com>
2024-05-04 17:23:08 +0000

    Fix formatting and typos in lmms_eval code

--- lmms_eval/models/__init__.py
+++ lmms_eval/models/__init__.py
@@ -1,30 +1,29 @@
 import os
+import hf_transfer
+
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
 AVAILABLE_MODELS = {
     "llava": "Llava",
-    # "qwen_vl": "Qwen_VL",
-    # "fuyu": "Fuyu",
-    # "gpt4v": "GPT4V",
-    # "instructblip": "InstructBLIP",
-    # "minicpm_v": "MiniCPM_V",
-    # "llava_vid": "LlavaVid",
-    # "videoChatGPT": "VideoChatGPT",
-    # "llama_vid": "LLaMAVid",
-    # "video_llava": "VideoLLaVA",
-    # "xcomposer2_4KHD": "XComposer2_4KHD",
-    # "claude": "Claude",
-    # "qwen_vl_api": "Qwen_VL_API",
-    # "llava_sglang": "LlavaSglang",
-    # "idefics2": "Idefics2",
-    "internvl":"InternVLChat"
+    "qwen_vl": "Qwen_VL",
+    "fuyu": "Fuyu",
+    "gpt4v": "GPT4V",
+    "instructblip": "InstructBLIP",
+    "minicpm_v": "MiniCPM_V",
+    "llava_vid": "LlavaVid",
+    "videoChatGPT": "VideoChatGPT",
+    "llama_vid": "LLaMAVid",
+    "video_llava": "VideoLLaVA",
+    "xcomposer2_4KHD": "XComposer2_4KHD",
+    "claude": "Claude",
+    "qwen_vl_api": "Qwen_VL_API",
+    "llava_sglang": "LlavaSglang",
+    "idefics2": "Idefics2",
+    "internvl": "InternVLChat",
 }
 
 for model_name, model_class in AVAILABLE_MODELS.items():
     try:
         exec(f"from .{model_name} import {model_class}")
     except ImportError:
-        print(model_name)
-
-import hf_transfer
-
-os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+        pass
