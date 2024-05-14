f3624ecdc6eef9c281f05a0af3767bfe3e12e984
Xiang Yue <yue.149@osu.edu>
2024-05-14 17:56:50 -0400

    revise the placeholder

--- M-MMMU/generate_placeholder.py
+++ M-MMMU/generate_placeholder.py
@@ -9,6 +9,35 @@ from PIL import Image
 
 def generate_placeholder_data(num_entries):
     data = []
+
+    # Real example 1 with id = example_1
+    example_1 = {
+        "id": "example_1",
+        "question": "Donna Donie, CFA, has a client who believes the common stock price of TRT Materials (currently $58 per share) could move substantially in either direction in reaction to an expected court decision involving the company. The client currently owns no TRT shares, but asks Donie for advice about implementing a strangle strategy to capitalize on the possible stock price movement. A strangle is a portfolio of a put and a call with different exercise prices but the same expiration date. Donie gathers the TRT option-pricing data: <image 1> Calculate, at expiration for long strangle strategy, the Maximum possible loss per share.",
+        "options": "['$9.00', '$5.00', 'the Maximum possible loss is unlimited']",
+        "explanation": "",
+        "img_type": "['Tables']",
+        "answer": "A",
+        "topic_difficulty": "Easy",
+        "question_type": "multiple-choice",
+        "subfield": "Investment"
+    }
+    data.append(example_1)
+
+    # Real example 2 with id = example_2
+    example_2 = {
+        "id": "example_2",
+        "question": "Among the following rests, which one does not have a compound dotted duration?",
+        "options": "['<image 1>', '<image 2>', '<image 3>', '<image 4>']",
+        "explanation": "",
+        "img_type": "['Sheet Music']",
+        "answer": "B",
+        "topic_difficulty": "Medium",
+        "question_type": "multiple-choice",
+        "subfield": "Music"
+    }
+    data.append(example_2)
+
     for i in range(1, num_entries + 1):
         entry = {
             "id": str(i),
