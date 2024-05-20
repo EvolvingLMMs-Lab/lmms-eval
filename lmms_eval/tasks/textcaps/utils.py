b2b4dbd2dc13432c79208db35abf7f55c97f1790
kcz358 <kaichenzhang358@outlook.com>
2024-05-20 07:45:11 +0000

    Comment out Spice in caption task so that don't need to download stanford nlp model

--- lmms_eval/tasks/textcaps/utils.py
+++ lmms_eval/tasks/textcaps/utils.py
@@ -38,7 +38,7 @@ def textcaps_process_result(doc, result):
 
 
 def textcaps_aggregation_result(results, metric, args=None):
-    scorers = [(Bleu(4), "Bleu_1"), (Bleu(4), "Bleu_2"), (Bleu(4), "Bleu_3"), (Bleu(4), "Bleu_4"), (Meteor(), "METEOR"), (Rouge(), "ROUGE_L"), (Cider(), "CIDEr"), (Spice(), "SPICE")]
+    scorers = [(Bleu(4), "Bleu_1"), (Bleu(4), "Bleu_2"), (Bleu(4), "Bleu_3"), (Bleu(4), "Bleu_4"), (Meteor(), "METEOR"), (Rouge(), "ROUGE_L"), (Cider(), "CIDEr")]#, (Spice(), "SPICE")]
     scorers_dict = {s[1]: s for s in scorers}
 
     stored_results = []
