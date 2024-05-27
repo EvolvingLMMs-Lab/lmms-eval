bd908ef144f0eb3ded23ef22e958c7553c4a0e71
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-05-27 10:49:05 +0800

    Update safety settings in GeminiAPI.generate_content() method

--- lmms_eval/models/gemini_api.py
+++ lmms_eval/models/gemini_api.py
@@ -139,7 +139,16 @@ class GeminiAPI(lmms):
 
             for attempt in range(5):
                 try:
-                    content = self.model.generate_content(message, generation_config=config, safety_settings={HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE})
+                    content = self.model.generate_content(
+                        message,
+                        generation_config=config,
+                        safety_settings={
+                            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+                            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+                            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+                            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+                        },
+                    )
                     content = content.text
                     break
                 except Exception as e:
