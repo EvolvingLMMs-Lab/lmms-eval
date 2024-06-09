fd85102252f66302657255d40f4cb4a6f3830c90
kcz358 <kaichenzhang358@outlook.com>
2024-06-09 13:01:20 +0000

    Add tools to calculate avg time in the video dataset

---
+++ tools/get_video_avg_time.py
@@ -0,0 +1,41 @@
+import json
+from lmms_eval.tasks import initialize_tasks, include_path, get_task_dict
+import av
+from tqdm import tqdm
+from av.codec.context import CodecContext
+
+tasks = ["worldqa_gen", "activitynetqa", "nextqa_oe_val", "nextqa_oe_test", "videochatgpt_gen", "egoschema"]
+# tasks = ["activitynetqa"]
+data_stats = {}
+if __name__ == "__main__":
+    initialize_tasks()
+
+    task_dict = get_task_dict(tasks, model_name="llavavid")
+    for task_name in task_dict.keys():
+        task_obj = task_dict[task_name]
+        if type(task_obj) == tuple:
+            group, task_obj = task_obj
+        
+        docs = task_obj.test_docs()
+        doc_to_visual = task_obj.doc_to_visual
+        data_stats[task_name] = 0
+        for doc in tqdm(docs, desc=f"Processing {task_name}"):
+            video_path = doc_to_visual(doc)
+            container = av.open(video_path[0])
+            if "webm" not in video_path[0] and "mkv" not in video_path[0]:
+                video = container.streams.video[0]
+                video_length = (float(video.duration * video.time_base)) # in seconds
+            else:
+                video_length = 0
+                # context = CodecContext.create("libvpx-vp9", "r")
+                for packet in container.demux(video=0):
+                    for frame in packet.decode():
+                        video_length = frame.time # The last frame time represent the video time
+            data_stats[task_name] += video_length
+        
+        data_stats[task_name] /= len(docs) # into seconds
+        # data_stats[task_name] /= 60 # into minutes
+    
+    with(open("./video_benchmarks_stats.json", "w")) as f:
+        json.dump(data_stats, f, indent=4, ensure_ascii=False)
+
