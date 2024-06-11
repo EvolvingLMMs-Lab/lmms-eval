1903fd42d38dcbe02efbbaa5f563d8e448fc387c
kcz358 <kaichenzhang358@outlook.com>
2024-06-11 06:35:48 +0000

    Use data packet to load video in pyav

--- lmms_eval/models/model_utils/load_video.py
+++ lmms_eval/models/model_utils/load_video.py
@@ -2,32 +2,33 @@ import av
 from av.codec.context import CodecContext
 import numpy as np
 
+# This one is faster
+def record_video_length_stream(container, indices):
+    frames = []
+    start_index = indices[0]
+    end_index = indices[-1]
+    for i, frame in enumerate(container.decode(video=0)):
+        if i > end_index:
+            break
+        if i >= start_index and i in indices:
+            frames.append(frame)
+    return frames
+
+# This one works for all types of video
+def record_video_length_packet(container):
+    frames = []
+    # context = CodecContext.create("libvpx-vp9", "r")
+    for packet in container.demux(video=0):
+        for frame in packet.decode(): 
+            frames.append(frame)
+    return frames
 
 def read_video_pyav(video_path, num_frm=8):
-    frames = []
     container = av.open(video_path)
 
-    if "webm" not in video_path and "mkv" not in video_path:
-        total_frames = container.streams.video[0].frames
-        num_frm = min(total_frames, num_frm)
-        indices = np.linspace(0, total_frames - 1, num_frm, dtype=int)
-        container.seek(0)
-        start_index = indices[0]
-        end_index = indices[-1]
-        for i, frame in enumerate(container.decode(video=0)):
-            if i > end_index:
-                break
-            if i >= start_index and i in indices:
-                frames.append(frame)
-    else:
-        # https://github.com/PyAV-Org/PyAV/issues/1269
-        # https://www.cnblogs.com/beyond-tester/p/17641872.html
-        # context = CodecContext.create("libvpx-vp9", "r")
-        for packet in container.demux(video=0):
-            for frame in packet.decode():
-                frames.append(frame)
-        total_frames = len(frames)
-        num_frm = min(total_frames, num_frm)
-        indices = np.linspace(0, total_frames - 1, num_frm, dtype=int)
-        frames = [frames[i] for i in indices]
+    frames = record_video_length_packet(container)
+    total_frames = len(frames)
+    num_frm = min(total_frames, num_frm)
+    indices = np.linspace(0, total_frames - 1, num_frm, dtype=int)
+    frames = [frames[i] for i in indices]
     return np.stack([x.to_ndarray(format="rgb24") for x in frames])
