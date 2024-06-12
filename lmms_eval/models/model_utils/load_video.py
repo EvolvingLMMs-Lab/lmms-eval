6ea976e5645094309192fb4cdf8c84142f20094d
kcz358 <kaichenzhang358@outlook.com>
2024-06-12 11:44:56 +0000

    Fix load video error for mp4 with packet

--- lmms_eval/models/model_utils/load_video.py
+++ lmms_eval/models/model_utils/load_video.py
@@ -29,25 +29,27 @@ def record_video_length_packet(container):
 
 
 def read_video_pyav(video_path, num_frm=8):
-    container = av.open(video_path)
-
+    
     if "webm" not in video_path and "mkv" not in video_path:
         # For mp4, we try loading with stream first
         try:
+            container = av.open(video_path)
             total_frames = container.streams.video[0].frames
-            num_frm = min(total_frames, num_frm)
-            indices = np.linspace(0, total_frames - 1, num_frm, dtype=int)
+            sampled_frm = min(total_frames, num_frm)
+            indices = np.linspace(0, total_frames - 1, sampled_frm, dtype=int)
             frames = record_video_length_stream(container, indices)
         except:
+            container = av.open(video_path)
             frames = record_video_length_packet(container)
             total_frames = len(frames)
-            num_frm = min(total_frames, num_frm)
-            indices = np.linspace(0, total_frames - 1, num_frm, dtype=int)
+            sampled_frm = min(total_frames, num_frm)
+            indices = np.linspace(0, total_frames - 1, sampled_frm, dtype=int)
             frames = [frames[i] for i in indices]
     else:
+        container = av.open(video_path)
         frames = record_video_length_packet(container)
         total_frames = len(frames)
-        num_frm = min(total_frames, num_frm)
-        indices = np.linspace(0, total_frames - 1, num_frm, dtype=int)
+        sampled_frm = min(total_frames, num_frm)
+        indices = np.linspace(0, total_frames - 1, sampled_frm, dtype=int)
         frames = [frames[i] for i in indices]
     return np.stack([x.to_ndarray(format="rgb24") for x in frames])
