6b1b1f576f20a0d0f1150d57133449f413acc4ed
Etash Guha <etashthebomb@gmail.com>
2024-06-18 12:10:22 -0700

    added ocrgpt4 and template

--- run_scripts/template.sh
+++ run_scripts/template.sh
@@ -7,19 +7,45 @@ if [[ -z "${OPENAI_API_KEY}" ]]; then
     exit 1      
 else 
     mkdir logs/${model_name}_${task_name}
-    sngpu  --time 105:59:59 --cpu 8 --mem 300000 --gpu 1 \
-    --output eval_logs/${model_name}_${task_name}.log -- \
-        "source /import/pa-tools/anaconda/anaconda3/2022-10/etc/profile.d/conda.sh &&  \
-        conda activate /import/ml-sc-scratch5/etashg/miniconda3/envs/mm_eval && \
-        export LD_LIBRARY_PATH=/import/ml-sc-scratch5/etashg/miniconda3/lib:$LD_LIBRARY_PATH && \
-        export OPENAI_API_KEY=$OPENAI_API_KEY && \
-        accelerate launch --num_processes 1 \
-        --config_file deepspeed_zero3_2workers.yaml -- \
-        lmms_eval --model ${model_name} \
-            --model_args ${model_args}  \
-            --tasks ${task_name} \
-            --batch_size 1 \
-            --log_samples \
-            --log_samples_suffix "${model_name}_${task_name}" \
-            --output_path logs/${model_name}_${task_name}"
+    if [[ $model_name == *"gpt"* || $model_name == *"claude"* ]]; then
+        source /import/pa-tools/anaconda/anaconda3/2022-10/etc/profile.d/conda.sh &&  \
+            export NCCL_DEBUG=INFO &&  \
+            export NCCL_DEBUG_SUBSYS=ALL &&  \
+            export NCCL_P2P_LEVEL=NVL &&  \
+            export NCCL_SHM_DISABLE=1 &&  \
+            export NCCL_IB_DISABLE=1 &&  \
+            conda activate /import/ml-sc-scratch5/etashg/miniconda3/envs/mm_eval && \
+            export LD_LIBRARY_PATH=/import/ml-sc-scratch5/etashg/miniconda3/lib:$LD_LIBRARY_PATH && \
+            export OPENAI_API_KEY=$OPENAI_API_KEY && \
+            lmms_eval --model ${model_name} \
+                --model_args ${model_args}  \
+                --tasks ${task_name} \
+                --batch_size 1 \
+                --log_samples \
+                --log_samples_suffix "${model_name}_${task_name}" \
+                --output_path logs/${model_name}_${task_name}
+    else
+        sngpu  --time 105:59:59 --cpu 8 --mem 300000 --gpu 1 \
+        --output eval_logs/${model_name}_${task_name}.log -- \
+            "source /import/pa-tools/anaconda/anaconda3/2022-10/etc/profile.d/conda.sh &&  \
+            export NCCL_DEBUG=INFO &&  \
+            export NCCL_DEBUG_SUBSYS=ALL &&  \
+            export NCCL_P2P_LEVEL=NVL &&  \
+            export NCCL_SHM_DISABLE=1 &&  \
+            export NCCL_IB_DISABLE=1 &&  \
+            conda activate /import/ml-sc-scratch5/etashg/miniconda3/envs/mm_eval && \
+            export LD_LIBRARY_PATH=/import/ml-sc-scratch5/etashg/miniconda3/lib:$LD_LIBRARY_PATH && \
+            export OPENAI_API_KEY=$OPENAI_API_KEY && \
+            accelerate launch --num_processes 1 \
+            --config_file deepspeed_zero3_2workers.yaml -- \
+            lmms_eval --model ${model_name} \
+                --model_args ${model_args}  \
+                --tasks ${task_name} \
+                --batch_size 1 \
+                --log_samples \
+                --log_samples_suffix "${model_name}_${task_name}" \
+                --output_path logs/${model_name}_${task_name}"
+    fi
+    
+    
 fi 
