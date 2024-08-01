27535fcb132f2e3d5e73c00bf34ce1968ad65ee7
Pu Fanyi <FPU001@e.ntu.edu.sg>
2024-07-31 22:05:42 -0700

    LiveBench Rollback (#152)
    * claude auto detect json mode
    
    * extract information
    
    * use claude to generate
    
    * fix bugs
    
    * fix
    
    * generate data
    
    * chore: Update dataset name and version for live_bench task
    
    * gpt-4-turbo => gpt-4o
    
    * chore: Update dataset capture settings in create_dataset.py
    
    * everything use gpt-4o
    
    * websites
    
    * livebench_july
    
    * Refactor code to simplify data assignment in example.ipynb
    
    * chore: Update dataset name for live_bench task
    
    * Update lmms_eval configuration to use process_results_use_image and full_docs options
    
    * split tasks
    
    * chore: Update live_bench task dataset names and versions
    
    * rollback

---
+++ tools/live_bench/live_bench/data_generator/score_getter.py
@@ -0,0 +1,157 @@
+import os
+import json
+import random
+import openai
+import anthropic
+from abc import ABC, abstractmethod
+from typing import List
+from PIL import Image
+from live_bench.screen_shoter import ScreenImage
+from live_bench.data_generator.qa_generator import Response
+from live_bench.data_generator.utils.gpt4v import format_gpt4v_images, gpt4v_generate_response
+from live_bench.data_generator.utils.claude import format_claude_images, claude_generate_response
+
+
+class Score(object):
+    def __init__(self, score: int, reason: str):
+        self.score = score
+        self.reason = reason
+
+
+class ScoreGetter(ABC):
+    def get_name(self):
+        return self.name
+
+    @abstractmethod
+    def get_score(self, question: str, answer: str, images: ScreenImage):
+        raise NotImplementedError("get_score not implemented")
+
+    def __call__(self, question: str, answer: str, images: ScreenImage, **kwargs):
+        return self.get_score(question, answer, images, **kwargs)
+
+
+class ScoreGetterRegistry:
+    def __init__(self):
+        self.score_getters = {}
+
+    def register_score_getter(self, name):
+        def decorator(cls):
+            self.score_getters[name] = cls
+            cls.name = name
+            return cls
+
+        return decorator
+
+    def get_score_getter(self, name) -> ScoreGetter:
+        return self.score_getters[name]
+
+    def get_random_score_getter(self) -> ScoreGetter:
+        return random.choice(list(self.score_getters.values()))
+
+
+generator_registry = ScoreGetterRegistry()
+
+
+def register_score_getter(name):
+    return generator_registry.register_score_getter(name)
+
+
+def get_score_getter(name, *args, **kwargs) -> ScoreGetter:
+    return generator_registry.get_score_getter(name)(*args, **kwargs)
+
+
+def get_random_score_getter(*args, **kwargs) -> ScoreGetter:
+    return generator_registry.get_random_score_getter()(*args, **kwargs)
+
+
+@register_score_getter("gpt4v")
+class GPT4VScoreGetter(ScoreGetter):
+    def __init__(self, prompt: str = os.path.join(os.path.dirname(__file__), "score_prompt.md"), model="gpt-4o", example_path=os.path.join(os.path.dirname(__file__), "example")):
+        super().__init__()
+        if os.path.exists(prompt):
+            with open(prompt, "r") as f:
+                self.prompt = f.read()
+        else:
+            self.prompt = prompt
+        API_KEY = os.getenv("OPENAI_API_KEY")
+        if not API_KEY:
+            raise ValueError("OPENAI_API_KEY environment variable not set.")
+        self.api_key = API_KEY
+        self.client = openai.OpenAI(api_key=self.api_key)
+        self.model = model
+        if os.path.exists(example_path) and os.path.isfile(os.path.join(example_path, "example_score_input.md")):
+            with open(example_path, "r") as f:
+                self.example = f.read()
+        else:
+            self.example = None
+
+    def _format_prompt(self, question: str, answer: str, images: List[Image.Image]):
+        prompt = [{"role": "system", "content": self.prompt}]
+        messages = []
+        for image in images:
+            messages.append(format_gpt4v_images(image))
+        messages.append({"type": "text", "text": f"Question: {question}\nQuestioner's Answer: {answer}"})
+        messages.append({"type": "text", "text": 'You should format you answer into json format like this: {"reason": "some reason", "score": 10}'})
+        prompt.append({"role": "user", "content": messages})
+        return prompt
+
+    def get_score(self, question: str, answer: str, images: ScreenImage, *, max_tokens=4096, max_try_times=5, **kwargs) -> Score:
+        prompt = self._format_prompt(question, answer, images)
+        try:
+            response = gpt4v_generate_response(client=self.client, model=self.model, messages=prompt, max_tokens=max_tokens, max_try_times=max_try_times, json_format=True, **kwargs)
+            if response.success:
+                content = json.loads(response.content)
+                score = content.get("score", None)
+                reason = content.get("reason", None)
+                return Score(score=score, reason=reason)
+            else:
+                return Score(score=None, reason=response.content)
+        except Exception as e:
+            return Score(score=None, reason=str(e))
+
+
+@register_score_getter("claude")
+class ClaudeScoreGetter(ScoreGetter):
+    def __init__(self, prompt: str = os.path.join(os.path.dirname(__file__), "score_prompt.md"), model="claude-3-5-sonnet-20240620", example_path=os.path.join(os.path.dirname(__file__), "example")):
+        super().__init__()
+        if os.path.exists(prompt):
+            with open(prompt, "r") as f:
+                self.prompt = f.read()
+        else:
+            self.prompt = prompt
+        API_KEY = os.getenv("ANTHROPIC_API_KEY")
+        if not API_KEY:
+            raise ValueError("ANTHROPIC_API_KEY environment variable not set.")
+        self.api_key = API_KEY
+        self.client = anthropic.Anthropic(api_key=self.api_key)
+        self.model = model
+        if os.path.exists(example_path) and os.path.isfile(os.path.join(example_path, "example_score_input.md")):
+            with open(example_path, "r") as f:
+                self.example = f.read()
+        else:
+            self.example = None
+
+    def _format_prompt(self, question: str, answer: str, images: List[Image.Image]):
+        # prompt = [{"role": "system", "content": self.prompt}]
+        prompt = []
+        messages = []
+        for image in images:
+            messages.append(format_claude_images(image))
+        messages.append({"type": "text", "text": f"Question: {question}\nQuestioner's Answer: {answer}"})
+        messages.append({"type": "text", "text": 'You should format you answer into JSON format like this: { "reason": "some reason", "score": 10 }'})
+        prompt.append({"role": "user", "content": messages})
+        return prompt
+
+    def get_score(self, question: str, answer: str, images: ScreenImage, *, max_tokens=4096, max_try_times=5, **kwargs) -> Score:
+        prompt = self._format_prompt(question, answer, images)
+        try:
+            response = claude_generate_response(client=self.client, model=self.model, messages=prompt, system=self.prompt, max_tokens=max_tokens, max_try_times=max_try_times, **kwargs)
+            if response.success:
+                content = json.loads(response.content)
+                score = content.get("score", None)
+                reason = content.get("reason", None)
+                return Score(score=score, reason=reason)
+            else:
+                return Score(score=None, reason=response.content)
+        except Exception as e:
+            return Score(score=None, reason=str(e))
---
+++ tools/live_bench/live_bench/screen_shoter/screen.py
@@ -0,0 +1,30 @@
+import io
+import base64
+
+from PIL import Image
+from typing import List, Tuple
+
+from live_bench.websites import Website
+
+
+def image_to_base64(image: Image.Image) -> str:
+    buffered = io.BytesIO()
+    image.save(buffered, format="PNG")
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+
+class ScreenImage(object):
+    def __init__(self, images: List[Image.Image], website: Website, shoter: str, screen_size: Tuple[int, int], capture_datetime: str):
+        self.images = images
+        self.website = website
+        self.shoter = shoter
+        self.screen_size = screen_size
+        self.capture_datetime = capture_datetime
+
+    def to_dict(self):
+        return {"images": self.images, "website": self.website.get_info(), "shoter": self.shoter, "screen_size": self.screen_size, "capture_datetime": self.capture_datetime}
+
+    def to_output_dict(self):
+        output = self.to_dict()
+        output["images"] = [image_to_base64(image) for image in self.images]
+        return output
