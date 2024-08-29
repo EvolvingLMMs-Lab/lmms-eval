fix bugs with loglikelihood
Signed-off-by: Jonathan Li <jonathanlingjieli+1@gmail.com>

lmms_eval/models/internvl2.py
@@ -618,13 +618,15 @@ class InternVL2(lmms):
                     labels=labels
                 )
             loss = outputs["loss"]
-            logits = outputs["logits"]
+            logits = torch.nn.functional.log_softmax(outputs["logits"], dim=-1)
             greedy_tokens = logits.argmax(dim=-1)
-            cont_toks = model_inputs["input_ids"][:, contxt_id.shape[1] + role_length:-1]
+            cont_toks = model_inputs["input_ids"][:, contxt_id.shape[1] + role_length + 1:-1]
             # account for off by 1 and im_end
-            greedy_toks = greedy_tokens[:, contxt_id.shape[1] + role_length - 1:-2]
+            greedy_toks = greedy_tokens[:, contxt_id.shape[1] + role_length:-2]
             max_equal = (greedy_toks == cont_toks).all()
-            res.append((float(loss.item()), bool(max_equal), self.tokenizer.decode(greedy_toks[0])))
+            cont_logits = logits[:, contxt_id.shape[1] + role_length:-2]
+            greedy_logits = torch.gather(cont_logits, 2, greedy_toks.unsqueeze(-1)).squeeze(-1)
+            res.append((float(greedy_logits.sum()), bool(max_equal), self.tokenizer.decode(greedy_toks[0])))
             pbar.update(1)
         pbar.update(1)
         return res

lmms_eval/models/llava_hf.py
@@ -224,13 +224,15 @@ class LlavaHf(lmms):
             with torch.inference_mode():
                 outputs = self.model(**model_inputs, labels=labels)
             loss = outputs["loss"]
-            logits = outputs["logits"]
+            logits = torch.nn.functional.log_softmax(outputs["logits"])
             greedy_tokens = logits.argmax(dim=-1)
             # account for the eos token
             cont_toks = model_inputs["input_ids"][:, contxt_id.shape[1] + 1:]  # [1, seq]
             greedy_tokens = greedy_tokens[:, contxt_id.shape[1] + 1 : model_inputs["input_ids"].shape[1]]  # [1, seq]
+            greedy_logits = logits[:, contxt_id.shape[1] + 1 : model_inputs["input_ids"].shape[1]]
+            greedy_logits = torch.gather(greedy_logits, 2, greedy_tokens.unsqueeze(-1)).squeeze(-1)
             max_equal = (greedy_tokens == cont_toks).all()
-            res.append((float(loss.item()), bool(max_equal), self.tokenizer.decode(greedy_tokens[0])))
+            res.append((float(greedy_logits.sum()), bool(max_equal), self.tokenizer.decode(greedy_tokens[0])))
             pbar.update(1)
 
         pbar.close()