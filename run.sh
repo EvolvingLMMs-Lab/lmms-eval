010302773a2769f6113ae6895ae38d50c5eb8b50
ASUS Desktop <1033718596@qq.com>
2024-09-20 11:54:51 -0700

    update datasets

--- run.sh
+++ run.sh
@@ -20,6 +20,26 @@ accelerate launch --num_processes=1 \
 
 accelerate launch --num_processes=1 \
 -m lmms_eval \
+--model llava_onevision \
+--model_args pretrained=lmms-lab/llava-onevision-qwen2-0.5b-si,conv_template=qwen_1_5,model_name=llava_qwen \
+--tasks mmmu,mme,mmbench,seedbench_2_plus,llava_wilder_small,llava_bench_coco,llava_in_the_wild,ferret,vibe_eval,wildvision \
+--batch_size 1 \
+--log_samples \
+--log_samples_suffix llava_onevision \
+--output_path ./logs/
+
+accelerate launch --num_processes=1 \
+-m lmms_eval \
+--model llava_onevision \
+--model_args pretrained=lmms-lab/llava-onevision-qwen2-0.5b-si,conv_template=qwen_1_5,model_name=llava_qwen \
+--tasks gqa,vqav2,ok_vqa,realworldqa \
+--batch_size 1 \
+--log_samples \
+--log_samples_suffix llava_onevision \
+--output_path ./logs/
+
+accelerate launch --num_processes=1 \
+-m lmms_eval \
 --model gpa_results \
 --model_args pretrained=lmms-lab/llava-onevision-qwen2-0.5b-si,conv_template=qwen_1_5,model_name=llava_qwen \
 --tasks gqa \
