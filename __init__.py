37d811b6a3a2be5b76817210803b012d6db801d6
Yuhao Dong <1342228579@qq.com>
2024-09-23 16:57:04 +0800

    update oryx model

---
+++ __init__.py
@@ -0,0 +1,73 @@
+import importlib
+import os
+import sys
+
+import hf_transfer
+from loguru import logger
+
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+
+logger.remove()
+logger.add(sys.stdout, level="WARNING")
+
+AVAILABLE_MODELS = {
+    "batch_gpt4": "BatchGPT4",
+    "claude": "Claude",
+    "cogvlm2": "CogVLM2",
+    "from_log": "FromLog",
+    "fuyu": "Fuyu",
+    "gemini_api": "GeminiAPI",
+    "gpt4v": "GPT4V",
+    "idefics2": "Idefics2",
+    "instructblip": "InstructBLIP",
+    "internvl": "InternVLChat",
+    "internvl2": "InternVL2",
+    "llama_vid": "LLaMAVid",
+    "llava": "Llava",
+    "llava_hf": "LlavaHf",
+    "llava_onevision": "Llava_OneVision",
+    "llava_sglang": "LlavaSglang",
+    "llava_vid": "LlavaVid",
+    "longva": "LongVA",
+    "mantis": "Mantis",
+    "minicpm_v": "MiniCPM_V",
+    "minimonkey": "MiniMonkey",
+    "mplug_owl_video": "mplug_Owl",
+    "phi3v": "Phi3v",
+    "qwen_vl": "Qwen_VL",
+    "qwen2_vl": "Qwen2_VL",
+    "qwen_vl_api": "Qwen_VL_API",
+    "reka": "Reka",
+    "srt_api": "SRT_API",
+    "tinyllava": "TinyLlava",
+    "videoChatGPT": "VideoChatGPT",
+    "video_llava": "VideoLLaVA",
+    "vila": "VILA",
+    "xcomposer2_4KHD": "XComposer2_4KHD",
+    "xcomposer2d5": "XComposer2D5",
+    "oryx": "Oryx",
+}
+
+
+def get_model(model_name):
+    if model_name not in AVAILABLE_MODELS:
+        raise ValueError(f"Model {model_name} not found in available models.")
+
+    model_class = AVAILABLE_MODELS[model_name]
+    try:
+        module = __import__(f"lmms_eval.models.{model_name}", fromlist=[model_class])
+        return getattr(module, model_class)
+    except Exception as e:
+        logger.error(f"Failed to import {model_class} from {model_name}: {e}")
+        raise
+
+
+if os.environ.get("LMMS_EVAL_PLUGINS", None):
+    # Allow specifying other packages to import models from
+    for plugin in os.environ["LMMS_EVAL_PLUGINS"].split(","):
+        m = importlib.import_module(f"{plugin}.models")
+        for model_name, model_class in getattr(m, "AVAILABLE_MODELS").items():
+            try:
+                exec(f"from {plugin}.models.{model_name} import {model_class}")
+            except ImportError as e:
+                logger.debug(f"Failed to import {model_class} from {model_name}: {e}")
