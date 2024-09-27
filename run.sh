2e8500834c29d6c189b0683fb321ca497a88513e
ASUS Desktop <1033718596@qq.com>
2024-09-27 11:29:13 -0700

    change api model name

--- run.sh
+++ run.sh
@@ -18,21 +18,24 @@ accelerate launch --num_processes=1 \
 --log_samples_suffix llava_onevision \
 --output_path ./logs/
 
+# ferret,wildvision
+# mmmu,mme,mmbench,seedbench_2_plus,llava_wilder_small,llava_bench_coco,llava_in_the_wild,vibe_eval
 accelerate launch --num_processes=1 \
 -m lmms_eval \
 --model llava_onevision \
 --model_args pretrained=lmms-lab/llava-onevision-qwen2-0.5b-si,conv_template=qwen_1_5,model_name=llava_qwen \
---tasks mmmu,mme,mmbench,seedbench_2_plus,llava_wilder_small,llava_bench_coco,llava_in_the_wild,ferret,vibe_eval,wildvision \
+--tasks llava_wilder_small \
 --batch_size 1 \
 --log_samples \
 --log_samples_suffix llava_onevision \
 --output_path ./logs/
 
+# gqa,vqav2,ok_vqa,realworldqa
 accelerate launch --num_processes=1 \
 -m lmms_eval \
 --model llava_onevision \
 --model_args pretrained=lmms-lab/llava-onevision-qwen2-0.5b-si,conv_template=qwen_1_5,model_name=llava_qwen \
---tasks gqa,vqav2,ok_vqa,realworldqa \
+--tasks realworldqa \
 --batch_size 1 \
 --log_samples \
 --log_samples_suffix llava_onevision \
