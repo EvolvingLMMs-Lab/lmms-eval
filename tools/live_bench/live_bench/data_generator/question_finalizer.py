7a56de8ba33e4546ab7f44843506825bdc597bff
Pu Fanyi <FPU001@e.ntu.edu.sg>
2024-10-09 15:13:32 +0800

    [Fix] LiveBench 2409 (#308)
    * Refactor code formatting in setup.py, .gitignore, __init__.py, and data_summary.ipynb
    
    * Refactor DefaultWebsite class in website.py
    
    * Refactor dataset name and limit processed images to 2
    
    * Refactor extract_infomation.py and update prompts
    
    * update
    
    * Refactor live_bench_2409.yaml and live_bench.yaml
    
    * Refactor live_bench/utils_v2.py and question_finalizer.py
    
    * Squashed commit of the following:
    
    commit e84f343aadb23ff9c183b8738eafd9a49a08e36f
    Author: Yinan He <43169235+yinanhe@users.noreply.github.com>
    Date:   Wed Oct 9 06:11:24 2024 +0200
    
        [Fix] Fix cache_dir issue where MVBench cannot be found (#306)
    
        * [add] add internvideo2 support && change mvbench to video branch
    
        * [add] answer_prompt of internvideo2
    
        * [add] change video type of internvideo2
    
        * [fix] update template of mvbench
    
        * [reformat]
    
        * [fix] generate_until_multi_round
    
        * [Feat] videochat2 support
    
        * [feat] Link cache_path to cache_dir if no unzip or untar
    
        * [feat] new variable in dataset kwargs: create_link
    
        ---------
    
        Co-authored-by: heyinan <heyinan@pjlab.org.cn>
    
    commit 87641e3e5a76e505732e5e130df2d26313e65178
    Author: GIO PAIK <giopaik0@gmail.com>
    Date:   Tue Oct 8 15:56:53 2024 +0900
    
        fix: Invalid group in mmsearch.yaml (#305)
    
    commit af87cc5ac553e7f170ccde710293fcaf23429010
    Author: Li Bo <drluodian@gmail.com>
    Date:   Tue Oct 8 08:42:20 2024 +0800
    
        [Doc] add more detailed task guide to explain the variables in yaml configuration file (#303)
    
    commit 3680ae49a0a715dff227fc8efdc439a5676d21d7
    Author: Pu Fanyi <FPU001@e.ntu.edu.sg>
    Date:   Tue Oct 8 01:25:35 2024 +0800
    
        [Feat] LiveBench 2409 (#304)
    
        * Refactor code formatting in setup.py, .gitignore, __init__.py, and data_summary.ipynb
    
        * Refactor DefaultWebsite class in website.py
    
        * Refactor dataset name and limit processed images to 2
    
        * Refactor extract_infomation.py and update prompts
    
        * update
    
        * Refactor live_bench_2409.yaml and live_bench.yaml
    
    commit ded426a509ef91c3c88e96886cd1274f88e8a510
    Author: Zhijian Liu <5782437+zhijian-liu@users.noreply.github.com>
    Date:   Sat Oct 5 11:46:44 2024 -0400
    
        Fix MMMU-Pro evaluation (#296)
    
    * fix

--- tools/live_bench/live_bench/data_generator/question_finalizer.py
+++ tools/live_bench/live_bench/data_generator/question_finalizer.py
@@ -74,34 +74,56 @@ class GeminiAnswerGetter(AnswerGetter):
 
 
 QUESTION_FINALIZER_PROMPT = """\
-You are a question setter, and your task is to modify the following question, answer, and scoring criteria to ensure:
+You are a question setter, and your task is to format answer the scoring criteria to ensure the scoring criteria is better for human reading:
+
+1. It should be a natural language, don't use dict / json format for the criteria, human cannot understand it.
+2. You can use bullet points / numbers to the list / yaml format to the criteria. But don't use python-like format.
+3. If the answer is in dict format, but there is no need to answer in dict format (means there is a way to answer in natural language, the question do not specify to answer in dict format), you should convert it to natural language.
+4. If the whole criteria is in other language, change it to English. But if you think some words should be in other language, you can keep it in that language. If question or answer is in other language, you don't need to change it.
+
+Ensure:
+1. The scoring criteria are rational and facilitate the accurate assessment of responses.
+2. The full score for the scoring criteria must be 10 points, and it must directly relate to the specific answer.
+3. The criteria should be in natural language instead of dict format. However, you can still use bullet points or numbers to list the criteria.
+4. You don't need to change the question, just format the answer and scoring criteria, but you still need to output the final question.
+5. Don't change the meaning of the question, answer, just format the answer and scoring criteria.
+"""
+
+"""3. The scoring criteria are rational and facilitate the accurate assessment of responses.
+4. The full score for the scoring criteria must be 10 points, and it must directly relate to the specific answer.
+5. The criteria should be in natural language instead of dict format. However, you can still use bullet points or numbers to list the criteria.
+Your task is to increase the difficulty of earning partial credit and ensure that the question and criteria are closely tied to the understanding of the image. What you need to keep in mind is that the original intent of this question is to assess the understanding of the image. Therefore, you can modify the question and criteria to make the scoring closely aligned with whether the image's content and details are correctly understood. Of course, while modifying the question and criteria, don't forget to adjust the answer to correspond with the revised question and criteria.
+
+Ensure:
 
 1. The question is clear and unambiguous.
 2. The answer is correct and reasonable (although the original ground truth answer is mostly correct, it may not be perfect, and sometimes the answer maybe incorrect).
 3. The scoring criteria are rational and facilitate the accurate assessment of responses.
 4. The full score for the scoring criteria must be 10 points, and it must directly relate to the specific answer.
-5. Except for some multiple-choice questions or other questions with only one possible answer, the scoring criteria should not be an all-or-nothing system. Partially correct answers should receive proportional points.
-6. Ensure that the scoring system is flexible enough to accommodate slight variations in correct answers while still maintaining a standard for what is considered an acceptable answer.
-7. Clearly define what constitutes a full score, partial score, and zero score.
-8. The criteria should be as detailed as possible, even if a LLM without image understanding capabilities could still score the answer based on the criteria and ground truth answer correctly.
+5. The criteria should be in natural language instead of dict format. However, you can still use bullet points or numbers to list the criteria.
+"""
+
+"""
 
 Your task is to finalize these standards, thus ensuring the correctness of the answer and the rationality of the scoring criteria.
 
 Some tips:
 
 1. For some extremely hard open-ended questions where answers may vary, hitting all points perfectly may not be realistic. In such cases, you can relax the criteria slightly. For example, if there are five possible points in an answer, but answering three adequately could merit full points. An other option is to change the question to a multiple-choice / multi-select question. But remember, it only applies to extremely hard open-ended questions which are impossible to answer perfectly.
-2. For some questions, changing the format might be beneficial. You can consider transforming them into different types of questions such as essay, fill-in-the-blank, multiple-choice (or multiple-select), true/false, ranking (e.g., based on time, importance, etc.), or matching questions to enhance the difficulty and rationality of the scoring criteria.
+2. For some questions, changing the format might be beneficial. You can consider transforming them into different types of questions such as essay, fill-in-the-blank, ranking (e.g., based on time, importance, etc.), or matching questions to enhance the difficulty and rationality of the scoring criteria. But a very important point is that DO NOT CHANGE the question to multiple-choice questions. If the original question is multiple-choice, you need to change it to another type of question (e.g., open-source, fill-in-the-blank, etc.).
+
+Very important: Do not change the question to a multiple-choice question. If the original question is multiple-choice, you need to change it to another type of question (e.g., open-source, fill-in-the-blank, etc.). If you changed the question type, please make sure you also adjust the answer and scoring criteria accordingly. For example, if a question is like a MCQ, but it don't have choices, you need to change it to a open-ended question, and adjust the answer and scoring criteria to fit the open-ended question.
 """
 
 FINALIZER_OUTPUT_FORMAT_PROMPT = """\
 Please provide the final question, answer, and scoring criteria in the following json format:
 {
-    "question": "The final question",
-    "answer": "The final answer",
-    "criteria": "The final scoring criteria"
+    "question": "<The final question>",
+    "answer": "<The final answer>",
+    "criteria": "<The final scoring criteria>"
 }
 
-One thing as a reminder is that if you want to add a new line in the json string, you should use the escape character "\\n" instead to represent the new line.
+<The final scoring criteria> should be a single string, not a dict / list object.
 """
 
 
@@ -114,16 +136,17 @@ class QuestionFinalizer(object):
 
     def finalize_question(self, question, answer, criteria, images: List[Image.Image]):
         information = [f"[Original Question]\n{question}", f"[Original Answer]\n{answer}", f"[Original Criteria]\n{criteria}"]
-        information.append(
-            "Below are answers from three candidates for reference. These answers may not be correct but are reasonably credible (but mostly correct). If any candidate rejects to answer, consider whether there is an issue with the question (such as containing violent or graphic content, or having a clear political bias). If so, please make necessary modifications. For open-ended questions, also consider the reasonableness of these answers. If they are reasonable, you may need to adjust the scoring criteria or the answer itself."
-        )
-        for model_name, model in self.models.items():
-            information.append(f"[{model_name} Answer]\n{model.get_answer(question, images)}")
+        # information.append(
+        #     "Below are answers from three candidates for reference. These answers may not be correct but are reasonably credible (but mostly correct). If any candidate rejects to answer, consider whether there is an issue with the question (such as containing violent or graphic content, or having a clear political bias). If so, please make necessary modifications. For open-ended questions, also consider the reasonableness of these answers. If they are reasonable, you may need to adjust the scoring criteria or the answer itself."
+        # )
+        # for model_name, model in self.models.items():
+        #     information.append(f"[{model_name} Answer]\n{model.get_answer(question, images)}")
         information.append(FINALIZER_OUTPUT_FORMAT_PROMPT)
         prompt = "\n\n".join(information)
         messages = [{"role": "user", "content": format_gpt4v_images(images) + [{"type": "text", "text": prompt}]}]
         try:
             response = gpt4v_generate_response(client=self.client, model="gpt-4o", messages=messages, system=QUESTION_FINALIZER_PROMPT)
+            # response = claude_generate_response(self.client, "claude-3-5-sonnet-20240620", messages)
             if response.success:
                 data = json.loads(response.content)
                 return {
