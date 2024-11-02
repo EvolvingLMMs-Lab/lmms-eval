aee28b9d92e1bf98614dc1adcabaf121da1d941f
kcz358 <kaichenzhang358@outlook.com>
2024-11-02 11:26:12 +0000

    Fix llava_hf generation eos token

--- lmms_eval/models/llava_sae_hooked.py
+++ lmms_eval/models/llava_sae_hooked.py
@@ -64,7 +64,6 @@ class LlavaSaeHooked(lmms):
         device_map: str = "",
         chat_template: Optional[str] = None,
         use_cache: bool = True,
-        specified_eot_token_id: Optional[int] = None,
         sae_path: Optional[str] = None,
         clamped_idx: int = 0,
         clamped_value: float = 50,
@@ -106,7 +105,6 @@ class LlavaSaeHooked(lmms):
         self.batch_size_per_gpu = int(batch_size)
         self.chat_template = chat_template
         self.use_cache = use_cache
-        self.specified_eot_token_id = specified_eot_token_id
         if accelerator.num_processes > 1 and device_map == "":
             assert accelerator.distributed_type in [DistributedType.FSDP, DistributedType.MULTI_GPU, DistributedType.DEEPSPEED], "Unsupported distributed type provided. Only DDP and FSDP are supported."
             # If you want to use DistributedType.DEEPSPEED, you have to run accelerate config before using the model
@@ -366,8 +364,8 @@ class LlavaSaeHooked(lmms):
                     num_beams=gen_kwargs["num_beams"],
                     max_new_tokens=gen_kwargs["max_new_tokens"],
                     use_cache=self.use_cache,
-                    pad_token_id=self.tokenizer.eos_token_id,
-                    eos_token_id=self.specified_eot_token_id,
+                    pad_token_id=self.eot_token_id,
+                    eos_token_id=self.eot_token_id,
                 )
                 cont = cont[:, inputs["input_ids"].shape[-1] :]
             except Exception as e:
