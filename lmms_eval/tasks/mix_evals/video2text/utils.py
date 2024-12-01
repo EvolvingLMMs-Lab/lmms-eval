cc232f7bd2f7606d28f70834e296e0b8e2eba427
Pu Fanyi <pufanyi@gmail.com>
2024-12-01 17:04:38 +0800

    video

--- lmms_eval/tasks/mix_evals/video2text/utils.py
+++ lmms_eval/tasks/mix_evals/video2text/utils.py
@@ -6,12 +6,13 @@ import sys
 import time
 from pathlib import Path
 
+import openai
 import requests
 import yaml
 from loguru import logger as eval_logger
 
 import lmms_eval.tasks._task_utils.file_utils as file_utils
-from lmms_eval.filters.extraction import ExtendedRegexFilter
+from lmms_eval.filters import Filter
 
 with open(Path(__file__).parent / "_default_template_yaml", "r") as f:
     raw_data = f.readlines()
@@ -26,73 +27,80 @@ with open(Path(__file__).parent / "_default_template_yaml", "r") as f:
 NUM_SECONDS_TO_SLEEP = 5
 GPT_EVAL_MODEL_NAME = config["metadata"]["gpt_eval_model_name"]
 API_TYPE = os.getenv("API_TYPE", "openai")
+API_VERSION = "gpt-3.5-turbo-0125"
+MAX_NEW_TOKENS = 999
 
 if API_TYPE == "openai":
-    API_URL = os.getenv("OPENAI_API_URL", "https://api.openai.com/v1/chat/completions")
-    API_KEY = os.getenv("OPENAI_API_KEY", "YOUR_API_KEY")
-    headers = {
-        "Authorization": f"Bearer {API_KEY}",
-        "Content-Type": "application/json",
-    }
+    client = openai.OpenAI()
 elif API_TYPE == "azure":
-    API_URL = os.getenv("AZURE_ENDPOINT", "https://api.cognitive.microsoft.com/sts/v1.0/issueToken")
-    API_KEY = os.getenv("AZURE_API_KEY", "YOUR_API_KEY")
-    headers = {
-        "api-key": API_KEY,
-        "Content-Type": "application/json",
-    }
-
-eval_prompt = """You are an AI assistant who will help me to evaluate the quality of a model response to a few candidate ground truth answers.
-
-Some criterion
-- Response that perfectly reflect the meaning of the ground truth: 1 point
-- Response that reflect none of the key points in the ground truth: 0 point
-- Some part in the response are correct but some parts in the ground truth are not mentioned in the response: 0.5 point
-- Some part in the response are correct but other parts in the response are not mentioned in the ground truth: 0.5 point
-
-Here're some examples about the scoring criterion and format:
-model response: Steam Cleaning Services
-ground truth: ["steam clean", "steam clean", "cleaning", "car", "steam clean"],
-Point: 1
-
-model response: A cowboy action shooter.
-ground truth: ["man"]
-Point: 1
-
-model response: I'm sorry, but I can't assist with that request.
-ground truth: ["quality"]
-Point: 0
-
-Let's begin this task:
-model response: {model_response}
-ground truth: {ground_truth}
-Point:"""
+    if "AZURE_ENDPOINT" in os.environ:
+        API_URL = os.getenv("AZURE_ENDPOINT", "https://api.cognitive.microsoft.com/sts/v1.0/issueToken")
+    else:
+        API_URL = os.getenv("AZURE_OPENAI_ENDPOINT", "https://api.cognitive.microsoft.com/sts/v1.0/issueToken")
+    if "AZURE_OPENAI_API_KEY" in os.environ:
+        API_KEY = os.getenv("AZURE_OPENAI_API_KEY", "YOUR_API_KEY")
+    else:
+        API_KEY = os.getenv("AZURE_API_KEY", "YOUR_API_KEY")
+    client = openai.AzureOpenAI(api_key=API_KEY, azure_endpoint=API_URL)
 
 
-def get_eval(model_response: str, ground_truth: str, max_tokens: int, retries: int = 5):
-    global headers
-    content = eval_prompt.format(model_response=model_response, ground_truth=ground_truth)
+video2text_gpt_judge_for_closeended_freeform = lambda prompt, gold_ans, response: [
+    {"role": "system", "content": f"In this task, I want you to act as a judge."},
+    {
+        "role": "user",
+        "content": f"""You will be provided with a question, its golden answer(s), and the model's answer, while the context of the question, which is one or more videos, is not given here. Your task is to judge how correct the model's answer is based on the golden answer(s), without seeing the input videos of the question, and then give a correctness score. The correctness score should be one of the below numbers: 0.0 (totally wrong), 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, or 1.0 (totally right). Your should first briefly give your reasoning process regarding how the model's answer conforms to or contradicts the golden answer(s), and then give the correctness score. The correctness score must strictly follow this format: \"[[score]]\", e.g., \"The correctness score: [[0.5]]\". Below are some examples. 
 
-    messages = [
-        {"role": "user", "content": content},
-    ]
+Example 1:
+Question: what does this video want to express
+Golden Answer(s): <answer 1> introduce method of playing
+Model's Answer: Volleyball serve \n
+Your Judgment: The model's answer "Volleyball serve" suggests a specific action, which may be part of what the video demonstrates. However, it misses the broader educational intent implied by the golden answer "introduce method of playing". Therefore, the answer is partially correct. The Correctness Score: [[0.5]]
+
+Example 2:
+Question: who do two other boys with surprised looks assist up?
+Golden Answer(s): <answer 1> boy
+Model's Answer: Boy.
+Your Judgment: The model's answer "Boy." precisely matches the golden answer which states the two other boys assist a "boy". The Correctness Score: [[1.0]]
+
+Example 3:
+Question: what did the lady do at the end of the video after their performance
+Golden Answer(s): <answer 1> picks up her phone
+Model's Answer: Nothing.
+Your Judgment: The model's answer "Nothing." directly contradicts the golden answer which states that the lady "picks up her phone" at the end of the video after their performance. Since the model's response completely misses the specific action described in the golden answer, it is incorrect. The Correctness Score: [[0.0]]
+
+Note that each one of the golden answers is considered correct. Thus if the model's answer matches any one of the golden answers, it should be considered correct. Judge the below case, give the brief reasoning process and the correctness score.
+
+Question: {prompt}
+Golden Answer(s): {gold_ans}
+Model's Answer: {response}
+Your Judgment: 
+""",
+    },
+]
+
+
+def get_eval(question, model_response: str, ground_truth: str, max_tokens: int, retries: int = 5):
+    global client
+    messages = video2text_gpt_judge_for_closeended_freeform(prompt=question, gold_ans=ground_truth, response=model_response)
 
     payload = {
         "model": GPT_EVAL_MODEL_NAME,
         "messages": messages,
-        "temperature": 0.2,
+        # "temperature": 0.2,
         "max_tokens": max_tokens,
     }
 
     for attempt in range(retries):
         try:
-            response = requests.post(API_URL, headers=headers, json=payload, timeout=60)
-            response.raise_for_status()
+            # response = requests.post(API_URL, headers=headers, json=payload, timeout=60)
+            response = client.chat.completions.create(**payload)
+            # response.raise_for_status()
             response_data = response.json()
 
             content = response_data["choices"][0]["message"]["content"].strip()
+            # content = response.choices[0].message.content.strip()
             if content != "":
-                return content, response_data["model"]
+                return content, response["model"]
             break  # If successful, break out of the loop
 
         except Exception as e:
@@ -156,7 +164,7 @@ def mix_evals_video2text_doc_to_text(doc, lmms_eval_specific_kwargs=None):
 
     user_prompt = doc["query"]
 
-    if "options" in doc:
+    if "options" in doc and len(doc["options"]) > 1:
         option_prompt = "Here are the options:\n"
         for idx, option in enumerate(doc["options"]):
             char_idx = chr(ord("A") + idx)
@@ -235,7 +243,7 @@ def mix_evals_video2text_process_results_freeform(doc, result):
     ground_truth_str = ", ".join([f'"{gt}"' for gt in doc["reference_answer"]])
     ground_truth_str = f"[{ground_truth_str}]"
     content = eval_prompt.format(model_response=pred, ground_truth=ground_truth_str)
-    eval_answer, model_name = get_eval(model_response=pred, ground_truth=ground_truth_str, max_tokens=1024)
+    eval_answer, model_name = get_eval(model_response=pred, ground_truth=ground_truth_str, max_tokens=MAX_NEW_TOKENS, question=doc["query"])
     return {
         "submission": {"pred": pred, "question_idx": doc["id"], "target": doc["reference_answer"], "eval_answer": eval_answer, "gpt_prompt": content},
         "gpt_eval": {"pred": pred, "question_idx": doc["id"], "target": doc["reference_answer"], "eval_answer": eval_answer, "gpt_prompt": content},
@@ -271,30 +279,114 @@ def mix_evals_video2text_aggregate_gen(results, args):
     mix_evals_video2text_aggregate_submissions(results, args, "OpenConvs")
 
 
-class MultiChoiceRegexFilter(ExtendedRegexFilter):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+video2text_gpt_judge_for_closeended_multiplechoice = lambda prompt, options, response: [
+    {"role": "system", "content": f"In this task, I want you to act as an option extractor."},
+    {
+        "role": "user",
+        "content": f"""You will be provided with a multiple-choice question, its options, and the model's answer, while the context of the question, which is one or more videos, is not given here. Your task is to extract or judge which option is chosen by the model based on its response, without seeing the context of the question. The extracted option should be one of the provided option letters. Your should first briefly give your reasoning process, and then give the extracted option letter. The extracted option must strictly follow this format: \"[[option letter]]\", e.g., \"The option chosen by the model: [[A]]\".
+Below are some examples. 
+
+Example 1:
+Question: What did he do to the car?
+Options:
+A. Paint the car
+B. Put plastic over the car
+C. Put metal over the car
+D. Cut the car
+Model's Answer: put plastic over the car.
+Your Judgment: The model's response directly aligns with option B, which is "Put plastic over the car." The response given is a paraphrase of this option without deviating in meaning. The option chosen by the model: [[B]]
+
+Example 2:
+Question: How did Eddie know Pam and Justin before Justin was killed?
+Options:
+A. They were part of the theater company
+B. They were high school friends
+C. They went to college together
+D. They were cousins
+E. They were siblings
+Model's Answer: A.
+Your Judgment: The model's answer directly provides the option letter "A." The option chosen by the model: [[A]]
+
+Example 3:
+Question: why do the people move in the same manner
+Options:
+A. uniform
+B. dancing with the baby
+C. exercising together
+D. stay together
+E. singing and dancing
+Model's Answer: sing and dance
+Your Judgment: The model's response "sing and dance" closely aligns with option E, which is "singing and dancing." The response provided is a direct paraphrase of this option, modifying only slightly the form of the words (from gerund to infinitive) but maintaining the same core activities described in the option. The option chosen by the model: [[E]]
+
+When you think that the model's answer does not match any of the given options, please choose the option that is the closest to the model's answer.
+Give the brief reasoning process and the extracted option for the below case. 
+
+Question: {prompt}
+Options: 
+{options}
+Model's Answer: {response}
+Your Judgment: 
+""",
+    },
+]
+
+
+class GPTMultiChoiceFilter(Filter):
+    def __init__(self, gpt_version: str = "gpt-3.5-turbo-0125", retries: int = 5):
+        """
+        Can define custom behavior here, if an individual instantiation of a Filter class should have state.
+        """
+        self.gpt_version = gpt_version
+
+        if API_TYPE == "openai":
+            self.client = openai.OpenAI(api_key=API_KEY)
+        elif API_TYPE == "azure":
+            self.client = openai.AzureOpenAI(api_key=API_KEY, azure_endpoint=API_URL)
+
+        self.retries = retries
 
     def apply(self, resps, docs):
-        filtered_resps = []
+        """
+        Defines the operation to perform on a list of the `inst.resps` properties of `Instance` objects.
+        Should return the list of (filtered) response lists *in the same order as they were input*, e.g.
+        if pass in [<inst.resps for instance 0>, <inst.resps for instance 1>] should return
+        [<filtered resps for instance 0>, <filtered resps for instance 1>]
+        """
+        results = []
+        for response, doc in zip(resps, docs):
+            query = doc["query"]
+            options = "\n".join([f"{chr(ord('A') + idx)}. {option}" for idx, option in enumerate(doc["options"])])
+            message = video2text_gpt_judge_for_closeended_multiplechoice(prompt=query, options=options, response=response)
+            payload = {
+                "model": self.gpt_version,
+                "messages": message,
+                "max_tokens": MAX_NEW_TOKENS,
+            }
+            result = 0
+            for attempt in range(self.retries):
+                try:
+                    # response = requests.post(API_URL, headers=headers, json=payload, timeout=60)
+                    response = client.chat.completions.create(**payload)
+                    # response.raise_for_status()
+                    response_data = response.json()
 
-        for r, doc in zip(resps, docs):
-            # Regex to directly extract the option letter from the model response
-            option_letter_regex = re.compile(r"\b([A-Z])\.\s+([^\n]*)")
+                    content = response_data["choices"][0]["message"]["content"].strip()
+                    # content = response.choices[0].message.content.strip()
+                    if content != "":
+                        match = re.search(r"r'\b([A-Z])\.?\b'", content)
+                        if match:
+                            result = ord(match.group(1)) - ord("A")
+                        else:
+                            result = 0
+                    break  # If successful, break out of the loop
 
-            # Process each response
-            filtered = []
-            for resp in r:
-                # Try to match the option letter at the start of the response
-                match = option_letter_regex.match(resp)
-                if match:
-                    # If a match is found, append the matched letter
-                    filtered.append(match.group(1))
-                else:
-                    # If no match, return the original response
-                    filtered.append(resp)
-
-            # Assuming we need the first response that matches or the original response
-            filtered_resps.append(filtered[0])
-
-        return filtered_resps
+                except Exception as e:
+                    eval_logger.info(f"Attempt {attempt + 1} failed with error: {e}")
+                    if attempt < self.retries:  # If we have retries left, sleep and then continue to next attempt
+                        time.sleep(NUM_SECONDS_TO_SLEEP)
+                    else:  # If this was the last attempt, log and return empty
+                        eval_logger.error(f"All {self.retries} attempts failed. Last error message: {e}")
+                        result = 0
+                        break
+            results.append(result)
+        return results
