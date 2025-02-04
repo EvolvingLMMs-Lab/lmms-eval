2e0010ffb9e9f0a216f88d763613b81902dd9a24
kcz358 <kaichenzhang358@outlook.com>
2025-02-04 00:33:39 +0000

    Add kino and kino_qwen2_5_vl
    Update
    
    Support navit processor
    
    Add pixels and error handle
    
    Resize for minimal smart resize
    
    Add kino qwen2_5
    
    Fix vdc 499 for azure
    
    Fix gpt model
    
    Revise kino logic for handling inputs
    
    Add error handl for vita
    
    Use qwen vl utils to process visual
    
    Fix model version

--- lmms_eval/tasks/mathvision/utils.py
+++ lmms_eval/tasks/mathvision/utils.py
@@ -11,7 +11,7 @@ from lmms_eval.tasks.mathvision.eval_utils import find_math_answer, is_equal, is
 
 NUM_SECONDS_TO_SLEEP = 5
 API_TYPE = os.getenv("API_TYPE", "openai")
-MODEL_VERSION = os.getenv("MODEL_VERSION", "gpt-4o-2024-08-06")
+MODEL_VERSION = os.getenv("MODEL_VERSION", "gpt-4o-2024-11-20")
 
 JUDGE_RULES = """You are a strict evaluator assessing answer correctness. You must output 1 for fully correct answers and 0 for any other case.
 # Input
