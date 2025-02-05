301f5e1ea88fda98d38ed5c40d9808849f72f8a5
ngquangtrung57 <quangtrung5705@gmail.com>
2025-02-06 00:26:12 +0800

    add covost2_en_zh task

--- lmms_eval/tasks/covost2/utils.py
+++ lmms_eval/tasks/covost2/utils.py
@@ -1,19 +1,25 @@
 import os
 import re
 import unicodedata
-
-import editdistance as ed  # TODO: new package
 from sacrebleu import corpus_bleu
-
+from lmms_eval.tasks.librispeech.cn_tn import TextNorm
 from lmms_eval.tasks.gigaspeech.whisper_normalizer.basic import BasicTextNormalizer
 from lmms_eval.tasks.gigaspeech.whisper_normalizer.english import EnglishTextNormalizer
 
-# ImportError: To support decoding audio files, please install 'librosa' and 'soundfile'.
 english_normalizer = EnglishTextNormalizer()
-
+chinese_normalizer = TextNorm(
+    to_banjiao=False,
+    to_upper=False,
+    to_lower=False,
+    remove_fillers=False,
+    remove_erhua=False,
+    check_chars=False,
+    remove_space=False,
+    cc_mode="",
+)
 basic_normalizer = BasicTextNormalizer()
 
-dir_name = os.path.dirname(os.path.abspath(__file__))
+PUNCS = "!,.?;:"
 
 
 def covost2_doc_to_audio(doc):
@@ -26,42 +32,70 @@ def covost2_doc_to_text(doc, lmms_eval_specific_kwargs):
     return f"{pre_prompt}{post_prompt}"
 
 
-def covost2_process_result(doc, result):
+def covost2_zh_en_process_result(doc, result):
     pred = result[0] if len(result) > 0 else ""
     gt = doc["gt"]
     data_dict = {"gt": gt, "pred": pred}
-
     return {"bleu": data_dict}
 
 
-PUNCS = "!,.?;:"
+def covost2_en_zh_process_result(doc, result):
+    pred = result[0] if len(result) > 0 else ""
+    gt = doc["translation"]
+    data_dict = {"gt": gt, "pred": pred}
+    return {"bleu": data_dict}
+
+
+def covost2_bleu(results, args):
+    refs, hyps = [], []
+    for result in results:
+        gt = result["gt"]
+        response = result["pred"]
+        gt = remove_sp(gt)
+        response = remove_sp(response)
+        refs.append(gt)
+        hyps.append(response)
+    bleu = compute_bleu_zh(refs, hyps)
+    return round(bleu, 5)
 
 
 def remove_sp(text):
     gt = re.sub(r"<\|.*?\|>", " ", text)
-    gt = re.sub(rf"\s+", r" ", gt)  # Replace consecutive spaces in the text with a single space.
+    gt = re.sub(rf"\s+", r" ", gt)
     gt = re.sub(f" ?([{PUNCS}])", r"\1", gt)
     gt = gt.lstrip(" ")
     return gt
 
 
+def compute_bleu_en(refs, hyps):
+    tokenizer = EvaluationTokenizer(
+        tokenizer_type="none",
+        lowercase=True,
+        punctuation_removal=True,
+        character_tokenization=False,
+    )
+    refs = [tokenizer.tokenize(english_normalizer(ref)) for ref in refs]
+    hyps = [tokenizer.tokenize(english_normalizer(hyp)) for hyp in hyps]
+    bleu_score = corpus_bleu(hyps, [refs], tokenize="13a")
+    return bleu_score.score
+
+
+def compute_bleu_zh(refs, hyps):
+    tokenizer = EvaluationTokenizer(
+        tokenizer_type="zh",
+        lowercase=True,
+        punctuation_removal=True,
+        character_tokenization=False,
+    )
+    refs = [tokenizer.tokenize(chinese_normalizer(ref)) for ref in refs]
+    hyps = [tokenizer.tokenize(chinese_normalizer(hyp)) for hyp in hyps]
+    bleu_score = corpus_bleu(hyps, [refs], tokenize="zh")
+    return bleu_score.score
+
+
 class EvaluationTokenizer(object):
-    """A generic evaluation-time tokenizer, which leverages built-in tokenizers
-    in sacreBLEU (https://github.com/mjpost/sacrebleu). It additionally provides
-    lowercasing, punctuation removal and character tokenization, which are
-    applied after sacreBLEU tokenization.
-
-    Args:
-        tokenizer_type (str): the type of sacreBLEU tokenizer to apply.
-        lowercase (bool): lowercase the text.
-        punctuation_removal (bool): remove punctuation (based on unicode
-        category) from text.
-        character_tokenization (bool): tokenize the text to characters.
-    """
-
     SPACE = chr(32)
     SPACE_ESCAPE = chr(9601)
-    # ALL_TOKENIZER_TYPES = ChoiceEnum(["none", "13a", "intl", "zh", "ja-mecab"])
 
     def __init__(
         self,
@@ -70,8 +104,6 @@ class EvaluationTokenizer(object):
         punctuation_removal: bool = False,
         character_tokenization: bool = False,
     ):
-        # from sacrebleu.tokenizers import TOKENIZERS
-        # from sacrebleu.tokenizers import tokenizer_none
         from sacrebleu.tokenizers.tokenizer_13a import Tokenizer13a
         from sacrebleu.tokenizers.tokenizer_char import TokenizerChar
         from sacrebleu.tokenizers.tokenizer_intl import TokenizerV14International
@@ -88,16 +120,14 @@ class EvaluationTokenizer(object):
             "char": TokenizerChar,
         }
 
-        assert tokenizer_type in TOKENIZERS, f"{tokenizer_type}, {TOKENIZERS}"
+        assert tokenizer_type in TOKENIZERS
         self.lowercase = lowercase
         self.punctuation_removal = punctuation_removal
         self.character_tokenization = character_tokenization
         self.tokenizer = TOKENIZERS[tokenizer_type]
-        # self.tokenizer = tokenizer_none
 
     @classmethod
     def remove_punctuation(cls, sent: str):
-        """Remove punctuation based on Unicode category."""
         return cls.SPACE.join(t for t in sent.split(cls.SPACE) if not all(unicodedata.category(c)[0] == "P" for c in t))
 
     def tokenize(self, sent: str):
@@ -113,32 +143,3 @@ class EvaluationTokenizer(object):
             tokenized = tokenized.lower()
 
         return tokenized
-
-
-def compute_bleu(refs, hyps):
-    tokenizer = EvaluationTokenizer(
-        tokenizer_type="none",
-        lowercase=True,
-        punctuation_removal=True,
-        character_tokenization=False,
-    )
-    refs = [tokenizer.tokenize(english_normalizer(ref)) for ref in refs]
-    hyps = [tokenizer.tokenize(english_normalizer(hyp)) for hyp in hyps]
-    # print(f"refs: {refs[0]}")
-    # print(f"pred: {hyps[0]}")
-    bleu_score = corpus_bleu(hyps, [refs], tokenize="13a")
-    return bleu_score.score
-
-
-def covost2_bleu(results, args):
-    refs, hyps = [], []
-    for result in results:
-        gt = result["gt"]
-        response = result["pred"]
-        gt = remove_sp(gt)
-        response = remove_sp(response)
-        refs.append(gt)
-        hyps.append(response)
-    bleu = compute_bleu(refs, hyps)
-    # print(f"source: {source}  cnt: {len(refs)} wer: {wer:.4f}")
-    return round(bleu, 5)
