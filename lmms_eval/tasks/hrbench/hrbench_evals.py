45f6507134b78806d888cc8de9b8cca5c28f979f
wenbin wang <>
2025-02-07 05:58:48 +0000

    add hrbench

--- lmms_eval/tasks/hrbench/hrbench_evals.py
+++ lmms_eval/tasks/hrbench/hrbench_evals.py
@@ -1,17 +1,16 @@
+import copy as cp
 import os
+import string
 import time
+from concurrent.futures import ThreadPoolExecutor
 
 import pandas as pd
 import requests
 from loguru import logger as eval_logger
 from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor
-import string
-import copy as cp
 
 
 class HRBenchEval:
-    
     API_TYPE = os.getenv("API_TYPE", "openai")
 
     if API_TYPE == "openai":
@@ -28,12 +27,12 @@ class HRBenchEval:
             "api-key": API_KEY,
             "Content-Type": "application/json",
         }
-        
-    def __init__(self, api_key, gpt_model="gpt-3.5-turbo",max_workers=12):
+
+    def __init__(self, api_key, gpt_model="gpt-3.5-turbo", max_workers=12):
         self.api_key = api_key
         self.gpt_model = gpt_model
-        self.max_workers=max_workers
-        
+        self.max_workers = max_workers
+
     def _post_request(self, payload):
         headers = {
             "Authorization": f"Bearer {self.api_key}",
@@ -42,24 +41,19 @@ class HRBenchEval:
         response = requests.post(self.API_URL, headers=headers, json=payload, timeout=30)
         response.raise_for_status()
         return response.json()
-    
-    def can_infer_option(self,answer, choices):
-        verbose = os.environ.get('VERBOSE', 0)
+
+    def can_infer_option(self, answer, choices):
+        verbose = os.environ.get("VERBOSE", 0)
         # Choices is a dictionary
-        if 'Failed to obtain answer via API' in answer:
+        if "Failed to obtain answer via API" in answer:
             return False
 
-        reject_to_answer = [
-            "Sorry, I can't help with images of people yet.",
-            "I can't process this file.",
-            "I'm sorry, but without the image provided",
-            'Cannot determine the answer'
-        ]
+        reject_to_answer = ["Sorry, I can't help with images of people yet.", "I can't process this file.", "I'm sorry, but without the image provided", "Cannot determine the answer"]
         for err in reject_to_answer:
             if err in answer:
-                return 'Z'
+                return "Z"
 
-        def count_choice(splits, choices, prefix='', suffix=''):
+        def count_choice(splits, choices, prefix="", suffix=""):
             cnt = 0
             for c in choices:
                 if prefix + c + suffix in splits:
@@ -67,23 +61,22 @@ class HRBenchEval:
             return cnt
 
         answer_mod = cp.copy(answer)
-        chars = '.()[],:;!*#{}'
+        chars = ".()[],:;!*#{}"
         for c in chars:
-            answer_mod = answer_mod.replace(c, ' ')
+            answer_mod = answer_mod.replace(c, " ")
 
         splits = [x.strip() for x in answer_mod.split()]
         count = count_choice(splits, choices)
 
         if count == 1:
             for ch in choices:
-                if 'A' in splits and len(splits) > 3 and verbose:
+                if "A" in splits and len(splits) > 3 and verbose:
                     return False
                 if ch in splits:
                     return ch
-        elif count == 0 and count_choice(splits, {'Z', ''}) == 1:
-            return 'Z'
+        elif count == 0 and count_choice(splits, {"Z", ""}) == 1:
+            return "Z"
         return False
-
 
     def can_infer_text(self, answer, choices):
         answer = answer.lower()
@@ -99,21 +92,20 @@ class HRBenchEval:
             return cands[0]
         return False
 
-    
     def can_infer(self, answer, choices):
         answer = str(answer)
         copt = self.can_infer_option(answer, choices)
         return copt if copt else self.can_infer_text(answer, choices)
-    
+
     def get_chat_response(self, data, temperature=0, max_tokens=256, patience=10, sleep_time=0):
-        question = data['question']
-        options = data['options']
-        prediction = data['prediction']
+        question = data["question"]
+        options = data["options"]
+        prediction = data["prediction"]
         ret = self.can_infer(prediction, options)
         if ret:
-            data['gpt_prediction'] = ret
+            data["gpt_prediction"] = ret
             return data
-        
+
         prompt = self.build_prompt(question, options, prediction)
         messages = [
             {"role": "user", "content": prompt},
@@ -127,7 +119,7 @@ class HRBenchEval:
                 prediction = response["choices"][0]["message"]["content"].strip()
                 if prediction and prediction != "" and "Failed to obtain answer via API" not in prediction:
                     ret = self.can_infer(prediction, options)
-                    data['gpt_prediction'] = ret
+                    data["gpt_prediction"] = ret
                     return data
 
             except Exception as e:
@@ -137,26 +129,25 @@ class HRBenchEval:
                 if sleep_time > 0:
                     time.sleep(sleep_time)
         return data
-    
+
     def build_prompt(self, question, options, prediction):
         options_prompt = ""
         for key, item in options.items():
-            options_prompt += f'{key}. {item}\n'
+            options_prompt += f"{key}. {item}\n"
         tmpl = (
-            'You are an AI assistant who will help me to match '
-            'an answer with several options of a single-choice question. '
-            'You are provided with a question, several options, and an answer, '
-            'and you need to find which option is most similar to the answer. '
-            'If the meaning of all options are significantly different from the answer, output Z. '
-            'Your should output a single uppercase character in A, B, C, D (if they are valid options), and Z. \n'
-            'Example 1: \n'
-            'Question: What is the main object in image?\nOptions: A. teddy bear B. rabbit C. cat D. dog\n'
-            'Answer: a cute teddy bear\nYour output: A\n'
-            'Example 2: \n'
-            'Question: What is the main object in image?\nOptions: A. teddy bear B. rabbit C. cat D. dog\n'
-            'Answer: Spider\nYour output: Z\n'
-            'Example 3: \n'
-            'Question: {}\nOptions: {}\nAnswer: {}\nYour output: '
+            "You are an AI assistant who will help me to match "
+            "an answer with several options of a single-choice question. "
+            "You are provided with a question, several options, and an answer, "
+            "and you need to find which option is most similar to the answer. "
+            "If the meaning of all options are significantly different from the answer, output Z. "
+            "Your should output a single uppercase character in A, B, C, D (if they are valid options), and Z. \n"
+            "Example 1: \n"
+            "Question: What is the main object in image?\nOptions: A. teddy bear B. rabbit C. cat D. dog\n"
+            "Answer: a cute teddy bear\nYour output: A\n"
+            "Example 2: \n"
+            "Question: What is the main object in image?\nOptions: A. teddy bear B. rabbit C. cat D. dog\n"
+            "Answer: Spider\nYour output: Z\n"
+            "Example 3: \n"
+            "Question: {}\nOptions: {}\nAnswer: {}\nYour output: "
         )
         return tmpl.format(question, options_prompt, prediction)
-    
--- lmms_eval/tasks/hrbench/utils.py
+++ lmms_eval/tasks/hrbench/utils.py
@@ -1,18 +1,19 @@
+import base64
 import datetime
+import io
 import json
 import os
-from collections import defaultdict
-import base64
-from PIL import Image
-import io
-import pandas as pd
 import string
-import yaml
+from collections import defaultdict
 from pathlib import Path
-from loguru import logger as eval_logger
-import numpy as np
-from lmms_eval.tasks.hrbench.hrbench_evals import HRBenchEval
 
+import numpy as np
+import pandas as pd
+import yaml
+from loguru import logger as eval_logger
+from PIL import Image
+
+from lmms_eval.tasks.hrbench.hrbench_evals import HRBenchEval
 
 with open(Path(__file__).parent / "hrbench.yaml", "r") as f:
     raw_data = f.readlines()
@@ -24,14 +25,16 @@ with open(Path(__file__).parent / "hrbench.yaml", "r") as f:
 
     config = yaml.safe_load("".join(safe_data))
 
-hrbench_evaluator = HRBenchEval(api_key=os.getenv("OPENAI_API_KEY", "YOUR_API_KEY"), gpt_model=config["metadata"]["gpt_eval_model_name"],max_workers=config["metadata"]['max_workers'])
+hrbench_evaluator = HRBenchEval(api_key=os.getenv("OPENAI_API_KEY", "YOUR_API_KEY"), gpt_model=config["metadata"]["gpt_eval_model_name"], max_workers=config["metadata"]["max_workers"])
+
 
 def decode_base64_to_image(base64_string, target_size=-1):
     image_data = base64.b64decode(base64_string)
-    image = Image.open(io.BytesIO(image_data)).convert('RGB')
+    image = Image.open(io.BytesIO(image_data)).convert("RGB")
     if target_size > 0:
         image.thumbnail((target_size, target_size))
     return image
+
 
 def hrbench_doc_to_visual(doc):
     image = decode_base64_to_image(doc["image"])
@@ -39,22 +42,18 @@ def hrbench_doc_to_visual(doc):
 
 
 def hrbench_doc_to_options(doc):
-    options = {
-        cand: doc[cand]
-        for cand in string.ascii_uppercase
-        if cand in doc and not pd.isna(doc[cand])
-    }
+    options = {cand: doc[cand] for cand in string.ascii_uppercase if cand in doc and not pd.isna(doc[cand])}
     return options
-    
+
 
 def hrbench_doc_to_text(doc, lmms_eval_specific_kwargs=None):
     question = doc["question"].strip()
     options = hrbench_doc_to_options(doc)
     options_prompt = ""
     for key, item in options.items():
-        options_prompt += f'{key}. {item}\n'
-    prompt = ''
-    prompt += f'{question}\n{options_prompt}Answer the option letter directly.' 
+        options_prompt += f"{key}. {item}\n"
+    prompt = ""
+    prompt += f"{question}\n{options_prompt}Answer the option letter directly."
     return prompt
 
 
@@ -69,32 +68,18 @@ def hrbench_process_results(doc, results):
     pred = results[0].strip()
     gt = doc["answer"]
     options = hrbench_doc_to_options(doc)
-    question = doc['question']
-    resp_dic = hrbench_evaluator.get_chat_response({
-        "question": question,
-        "options": options,
-        "prediction": pred
-    })
-    gpt_prediction = resp_dic['gpt_prediction']
-    category = doc['category']
-    cycle_category = doc['cycle_category']
-    
+    question = doc["question"]
+    resp_dic = hrbench_evaluator.get_chat_response({"question": question, "options": options, "prediction": pred})
+    gpt_prediction = resp_dic["gpt_prediction"]
+    category = doc["category"]
+    cycle_category = doc["cycle_category"]
+
     gpt_score = 0
     if gt.lower() == gpt_prediction.lower():
         gpt_score = 1
-        
-    return {
-        category:{
-            "index": doc['index'],
-            "cycle_category": cycle_category,
-            "gpt_score": gpt_score
-        },
-        "average":{
-            "index": doc['index'],
-            "cycle_category": cycle_category,
-            "gpt_score": gpt_score
-        }
-    }
+
+    return {category: {"index": doc["index"], "cycle_category": cycle_category, "gpt_score": gpt_score}, "average": {"index": doc["index"], "cycle_category": cycle_category, "gpt_score": gpt_score}}
+
 
 def hrbench_aggregate_results(results, args):
     """
@@ -105,14 +90,14 @@ def hrbench_aggregate_results(results, args):
     """
     cycle_category_scores = defaultdict(list)
     for result in results:
-        score = result['gpt_score']
-        cycle_category = result['cycle_category']
+        score = result["gpt_score"]
+        cycle_category = result["cycle_category"]
         cycle_category_scores[cycle_category].append(score)
-    
+
     cycle_category_avg_score = {}
     for cycle_category, scores in cycle_category_scores.items():
         avg_score = sum(scores) / len(scores)
         cycle_category_avg_score[cycle_category] = avg_score
-        
+
     avg_score = sum(cycle_category_avg_score.values()) / len(cycle_category_avg_score)
     return avg_score
