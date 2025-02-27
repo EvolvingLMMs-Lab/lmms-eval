765ef79f04d8a3991beecb4dbe3aa711557bacf1
Yan Shu <570533048@qq.com>
2025-02-27 10:51:05 +0100

    Update utils.py

--- lmms_eval/tasks/mlvu/utils.py
+++ lmms_eval/tasks/mlvu/utils.py
@@ -14,7 +14,7 @@ from loguru import logger as eval_logger
 
 from lmms_eval.tasks._task_utils.file_utils import generate_submission_file
 
-TASK_TYPES = ["TR", "AR", "VS", "NQA", "ER", "PQA", "SSC", "AO", "AC"]
+TASK_TYPES = ["TR", "AR", "NQA", "ER", "PQA", "AO", "AC"]
 
 
 hf_home = os.getenv("HF_HOME", "./~/.cache/huggingface")
