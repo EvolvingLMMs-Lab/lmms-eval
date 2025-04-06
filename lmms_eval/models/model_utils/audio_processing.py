9501e6456321246c4ac37ae0dbb97b474dab7cf4
Kaichen Zhang - NTU <kaichenzhang358@outlook.com>
2025-04-06 17:45:55 +0800

    [Feat] Adding libri long (#618)
    * Add librispeech long
    
    * Add split audio to qwen

--- lmms_eval/models/model_utils/audio_processing.py
+++ lmms_eval/models/model_utils/audio_processing.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 from librosa import resample
 
@@ -5,3 +7,23 @@ from librosa import resample
 def downsample_audio(audio_array: np.ndarray, original_sr: int, target_sr: int) -> np.ndarray:
     audio_resample_array = resample(audio_array, orig_sr=original_sr, target_sr=target_sr)
     return audio_resample_array
+
+
+def split_audio(audio_arrays: np.ndarray, chunk_lim: int) -> List:
+    """
+    Splits the audio into chunks of a given length.
+    Args:
+        audio_arrays: The audio array to split.
+        chunk_lim: The length of each chunk.
+    Returns:
+        A list of audio chunks.
+    """
+    audio_splits = []
+    # Split the loaded audio to 30s chunks and extend the messages content
+    for i in range(
+        0,
+        len(audio_arrays),
+        chunk_lim,
+    ):
+        audio_splits.append(audio_arrays[i : i + chunk_lim])
+    return audio_splits
