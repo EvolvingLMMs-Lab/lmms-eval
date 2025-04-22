0ae0b9f801bdf40d5a399730672a242499ba2411
Kaichen Zhang - NTU <kaichenzhang358@outlook.com>
2025-04-22 15:26:16 +0800

    [Fix] Better Qwen omni and linting (#647)
    * Update omni for better usability
    
    * Formatting

--- lmms_eval/tasks/vsibench/utils.py
+++ lmms_eval/tasks/vsibench/utils.py
@@ -1,13 +1,12 @@
-
 import os
-from pathlib import Path
-import yaml
-from loguru import logger as eval_logger
 from functools import partial
-import numpy as np
-import pandas as pd
+from pathlib import Path
 
 import datasets
+import numpy as np
+import pandas as pd
+import yaml
+from loguru import logger as eval_logger
 
 MCA_QUESTION_TYPES = [
     "object_rel_direction_easy",
@@ -57,13 +56,13 @@ def vsibench_doc_to_visual(doc):
 
 def vsibench_doc_to_text(doc, lmms_eval_specific_kwargs=None):
     question = doc["question"]
-        
+
     pre_prompt = lmms_eval_specific_kwargs.get("pre_prompt", "") or "These are frames of a video."
-    
-    if doc['question_type'] in NA_QUESTION_TYPES:
+
+    if doc["question_type"] in NA_QUESTION_TYPES:
         post_prompt = lmms_eval_specific_kwargs.get("na_post_prompt", "") or "Please answer the question using a single word or phrase."
         return pre_prompt + "\n" + question + "\n" + post_prompt
-    elif doc['question_type'] in MCA_QUESTION_TYPES:
+    elif doc["question_type"] in MCA_QUESTION_TYPES:
         options = "Options:\n" + "\n".join(doc["options"])
         post_prompt = lmms_eval_specific_kwargs.get("mca_post_prompt", "") or "Answer with the option's letter from the given choices directly."
         return "\n".join([pre_prompt, question, options, post_prompt])
@@ -72,19 +71,23 @@ def vsibench_doc_to_text(doc, lmms_eval_specific_kwargs=None):
 
 
 def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
-    if os.getenv('LMMS_EVAL_SHUFFLE_DOCS', None):
+    if os.getenv("LMMS_EVAL_SHUFFLE_DOCS", None):
         eval_logger.info(f"Environment variable LMMS_EVAL_SHUFFLE_DOCS detected, dataset will be shuffled.")
         return dataset.shuffle(seed=42)
     return dataset
 
+
 def fuzzy_matching(pred):
-    return pred.split(' ')[0].rstrip('.').strip()
+    return pred.split(" ")[0].rstrip(".").strip()
+
 
 def exact_match(pred, target):
-    return 1. if pred.lower() == target.lower() else 0.
+    return 1.0 if pred.lower() == target.lower() else 0.0
+
 
 def abs_dist_norm(pred, target):
     return abs(pred - target) / target
+
 
 def mean_relative_accuracy(pred, target, start, end, interval):
     num_pts = (end - start) / interval + 2
@@ -92,10 +95,12 @@ def mean_relative_accuracy(pred, target, start, end, interval):
     accuracy = abs_dist_norm(pred, target) <= 1 - conf_intervs
     return accuracy.mean()
 
+
 WORST_CASE_FOR_METRICS = {
-    "accuracy": 0.,
-    "MRA:.5:.95:.05": 0.,
+    "accuracy": 0.0,
+    "MRA:.5:.95:.05": 0.0,
 }
+
 
 def to_float(pred):
     try:
@@ -104,16 +109,16 @@ def to_float(pred):
         pred = None
     return pred
 
+
 def vsibench_process_results(doc, results):
-    
-    doc['prediction'] = results[0]
-    if doc['question_type'] in MCA_QUESTION_TYPES:
+    doc["prediction"] = results[0]
+    if doc["question_type"] in MCA_QUESTION_TYPES:
         for key, value in METRICS_FOR_MCA.items():
-            doc[key] = eval(value)(fuzzy_matching(doc['prediction']), doc['ground_truth'])
-    elif doc['question_type'] in NA_QUESTION_TYPES:
+            doc[key] = eval(value)(fuzzy_matching(doc["prediction"]), doc["ground_truth"])
+    elif doc["question_type"] in NA_QUESTION_TYPES:
         for key, value in METRICS_FOR_NA.items():
             try:
-                doc[key] = eval(value)(to_float(fuzzy_matching(doc['prediction'])), to_float(doc['ground_truth']))
+                doc[key] = eval(value)(to_float(fuzzy_matching(doc["prediction"])), to_float(doc["ground_truth"]))
             except TypeError:
                 doc[key] = WORST_CASE_FOR_METRICS[key]
     else:
@@ -121,33 +126,39 @@ def vsibench_process_results(doc, results):
 
     return {"vsibench_score": doc}
 
+
 def vsibench_aggregate_results(results):
     results = pd.DataFrame(results)
-    
+
     output = {}
 
-    for question_type, question_type_indexes in results.groupby('question_type').groups.items():
+    for question_type, question_type_indexes in results.groupby("question_type").groups.items():
         per_question_type = results.iloc[question_type_indexes]
-        
+
         if question_type in MCA_QUESTION_TYPES:
             for metric in METRICS_FOR_MCA.keys():
                 output[f"{question_type}_{metric}"] = per_question_type[metric].mean()
         elif question_type in NA_QUESTION_TYPES:
             for metric in METRICS_FOR_NA.keys():
-                if metric == 'success_rate':
+                if metric == "success_rate":
                     output[f"{question_type}_{metric}"] = per_question_type[metric].mean()
                 else:
                     output[f"{question_type}_{metric}"] = per_question_type[metric].mean()
 
         else:
             raise ValueError(f"Unknown question type: {question_type}")
-    
-    output['object_rel_direction_accuracy'] = sum([
-        output.pop('object_rel_direction_easy_accuracy'),
-        output.pop('object_rel_direction_medium_accuracy'),
-        output.pop('object_rel_direction_hard_accuracy'),
-    ]) / 3.
-    
-    output['overall'] = sum([_ for _ in output.values()]) / len(output)
+
+    output["object_rel_direction_accuracy"] = (
+        sum(
+            [
+                output.pop("object_rel_direction_easy_accuracy"),
+                output.pop("object_rel_direction_medium_accuracy"),
+                output.pop("object_rel_direction_hard_accuracy"),
+            ]
+        )
+        / 3.0
+    )
+
+    output["overall"] = sum([_ for _ in output.values()]) / len(output)
     eval_logger.info(f"Evaluation results: {output}")
     return output
