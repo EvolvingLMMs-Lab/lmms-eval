de08ff40a5ad236e2f76d3628b3ac2c5efb349a0
Bo Li <drluodian@gmail.com>
2025-05-24 05:16:00 +0000

    Update project configuration and scripts
    - Updated Python version requirement in pyproject.toml to >=3.12.
    - Removed specific version constraint for protobuf in dependencies.
    - Added 'uv.lock' to .gitignore.
    - Modified example script to change model task from 'mmmu_pro' to 'mme' and updated comments for clarity.

--- examples/models/qwen25vl.sh
+++ examples/models/qwen25vl.sh
@@ -3,16 +3,16 @@
 export HF_HOME="~/.cache/huggingface"
 # pip install git+https://github.com/EvolvingLMMs-Lab/lmms-eval.git
 # pip3 install qwen_vl_utils
-# use `interleave_visuals=True` to control the visual token position
+# use `interleave_visuals=True` to control the visual token position, currently only for mmmu_val and mmmu_pro
 
-accelerate launch --num_processes=8 --main_process_port=12346 -m lmms_eval \
-    --model qwen2_vl \
-    --model_args=pretrained=Qwen/Qwen2-VL-7B-Instruct,max_pixels=12845056,use_flash_attention_2=True,interleave_visuals=True \
-    --tasks mmmu_pro \
-    --batch_size 1
+# accelerate launch --num_processes=8 --main_process_port=12346 -m lmms_eval \
+#     --model qwen2_vl \
+#     --model_args=pretrained=Qwen/Qwen2-VL-7B-Instruct,max_pixels=12845056,use_flash_attention_2=True,interleave_visuals=True \
+#     --tasks mmmu_pro \
+#     --batch_size 1
 
 accelerate launch --num_processes=8 --main_process_port=12346 -m lmms_eval \
     --model qwen2_5_vl \
-    --model_args=pretrained=Qwen/Qwen2.5-VL-7B-Instruct,max_pixels=12845056,use_flash_attention_2=True,interleave_visuals=True \
-    --tasks mmmu_pro \
+    --model_args=pretrained=Qwen/Qwen2.5-VL-7B-Instruct,max_pixels=12845056,use_flash_attention_2=True,interleave_visuals=False \
+    --tasks mme \
     --batch_size 1
