110de2853ef8da9889cf7e53a0cd1825e1e4b245
Li Bo <drluodian@gmail.com>
2025-06-08 12:34:42 +0800

    Remove pdb from test_llava and add output assertions

--- miscs/test_llava.py
+++ miscs/test_llava.py
@@ -9,13 +9,26 @@ prompt_1 = "USER: <image>\nWhat does this image show?\nASSISTANT:"
 prompt_2 = "USER: <image> <image> \nWhat is the difference between these two images?\nASSISTANT:"
 image_file_1 = "image1.png"
 image_file_2 = "image2.png"
-model = LlavaForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True, use_flash_attention_2=True).to(0)
-processor = AutoProcessor.from_pretrained(model_id)
-raw_image_1 = Image.open(image_file_1)
-raw_image_2 = Image.open(image_file_2)
-inputs = processor([prompt_1, prompt_2], [raw_image_1, raw_image_1, raw_image_2], padding=True, return_tensors="pt").to(0, torch.float16)
-import pdb
 
-pdb.set_trace()
-output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
-print(processor.batch_decode(output, skip_special_tokens=True))
+
+def test_llava_generation():
+    model = LlavaForConditionalGeneration.from_pretrained(
+        model_id,
+        torch_dtype=torch.float16,
+        low_cpu_mem_usage=True,
+        use_flash_attention_2=True,
+    ).to(0)
+    processor = AutoProcessor.from_pretrained(model_id)
+    raw_image_1 = Image.open(image_file_1)
+    raw_image_2 = Image.open(image_file_2)
+    inputs = processor(
+        [prompt_1, prompt_2],
+        [raw_image_1, raw_image_1, raw_image_2],
+        padding=True,
+        return_tensors="pt",
+    ).to(0, torch.float16)
+    output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
+    decoded = processor.batch_decode(output, skip_special_tokens=True)
+    print(decoded)
+    assert len(decoded) == 2
+    assert all(isinstance(s, str) for s in decoded)
