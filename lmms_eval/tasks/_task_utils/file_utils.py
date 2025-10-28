7611727a3e81cd4e392fb834ea85703db796dd8d
Kyunnilee <annelee5270@gmail.com>
2025-10-28 13:07:54 +0000

    file upload bug fix

--- lmms_eval/tasks/_task_utils/file_utils.py
+++ lmms_eval/tasks/_task_utils/file_utils.py
@@ -2,11 +2,11 @@ import os
 
 
 def generate_submission_file(file_name, args, subpath="submissions"):
-    if args is None or args.output_path is None:
+    if args is None or args.output is None:
         # If no output path is specified, use current directory
         path = subpath
     else:
-        path = os.path.join(args.output_path, subpath)
+        path = os.path.join(args.output, subpath)
     os.makedirs(path, exist_ok=True)
     path = os.path.join(path, file_name)
     return os.path.abspath(path)
