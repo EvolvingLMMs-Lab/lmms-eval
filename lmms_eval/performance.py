cc7d15c9758ad14b04779edbf54c19d3e9c60920
Bo Li <drluodian@gmail.com>
2026-08-23 13:41:29 +0800

    perf: add baseline phase recorder

---
+++ lmms_eval/performance.py
@@ -0,0 +1,4 @@
+from lmms_eval._performance.legacy_invocation import redact_secrets
+from lmms_eval._performance.recorder import BaselinePerformanceRecorder
+
+__all__ = ["BaselinePerformanceRecorder", "redact_secrets"]
